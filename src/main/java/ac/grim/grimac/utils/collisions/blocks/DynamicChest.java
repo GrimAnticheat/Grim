74b89326fdc1dc55430548a1b10784f53f19e0d9
MWHunter <s0521458@student.rockvalleycollege.edu>
2021-04-27 18:03:56 -0500

    1.13+ Chest collision support.
    1.7-1.12 support requires reading the world

--- src/main/java/ac/grim/grimac/utils/collisions/blocks/DynamicChest.java
+++ src/main/java/ac/grim/grimac/utils/collisions/blocks/DynamicChest.java
@@ -2,9 +2,12 @@ package ac.grim.grimac.utils.collisions.blocks;
 
 import ac.grim.grimac.utils.collisions.CollisionBox;
 import ac.grim.grimac.utils.collisions.types.CollisionFactory;
+import ac.grim.grimac.utils.collisions.types.HexCollisionBox;
 import ac.grim.grimac.utils.collisions.types.SimpleCollisionBox;
 import ac.grim.grimac.utils.data.ProtocolVersion;
+import org.bukkit.block.BlockFace;
 import org.bukkit.block.data.BlockData;
+import org.bukkit.block.data.type.Chest;
 
 // In 1.12, chests don't have data that say what type of chest they are, other than direction
 public class DynamicChest implements CollisionFactory {
@@ -15,6 +18,20 @@ public class DynamicChest implements CollisionFactory {
 
     @Override
     public CollisionBox fetch(ProtocolVersion version, BlockData block, int x, int y, int z) {
-        return new SimpleCollisionBox(0, 0, 0, 1, 1, 1);
+        Chest chest = (Chest) block;
+
+        if (chest.getType() == Chest.Type.SINGLE) {
+            return new HexCollisionBox(1.0D, 0.0D, 1.0D, 15.0D, 14.0D, 15.0D);
+        }
+
+        if (chest.getFacing() == BlockFace.SOUTH && chest.getType() == Chest.Type.RIGHT || chest.getFacing() == BlockFace.NORTH && chest.getType() == Chest.Type.LEFT) {
+            return new HexCollisionBox(1.0D, 0.0D, 1.0D, 16.0D, 14.0D, 15.0D); // Connected to the east face
+        } else if (chest.getFacing() == BlockFace.SOUTH && chest.getType() == Chest.Type.LEFT || chest.getFacing() == BlockFace.NORTH && chest.getType() == Chest.Type.RIGHT) {
+            return new HexCollisionBox(0.0D, 0.0D, 1.0D, 15.0D, 14.0D, 15.0D); // Connected to the west face
+        } else if (chest.getFacing() == BlockFace.WEST && chest.getType() == Chest.Type.RIGHT || chest.getFacing() == BlockFace.EAST && chest.getType() == Chest.Type.LEFT) {
+            return new HexCollisionBox(1.0D, 0.0D, 1.0D, 15.0D, 14.0D, 16.0D); // Connected to the south face
+        } else { // This is correct
+            return new HexCollisionBox(1.0D, 0.0D, 0.0D, 15.0D, 14.0D, 15.0D); // Connected to the north face
+        }
     }
 }
