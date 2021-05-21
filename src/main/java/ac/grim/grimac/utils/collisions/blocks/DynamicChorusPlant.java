df41b1edb8db831215321a6005374fd85fb8f45b
MWHunter <s0521458@student.rockvalleycollege.edu>
2021-05-21 13:35:53 -0500

    Chorus plants - finish 1.12 on 1.13, 1.13 on 1.12, 1.12 on 1.12

--- src/main/java/ac/grim/grimac/utils/collisions/blocks/DynamicChorusPlant.java
+++ src/main/java/ac/grim/grimac/utils/collisions/blocks/DynamicChorusPlant.java
@@ -7,12 +7,23 @@ import ac.grim.grimac.utils.collisions.datatypes.CollisionBox;
 import ac.grim.grimac.utils.collisions.datatypes.CollisionFactory;
 import ac.grim.grimac.utils.collisions.datatypes.ComplexCollisionBox;
 import ac.grim.grimac.utils.collisions.datatypes.SimpleCollisionBox;
+import ac.grim.grimac.utils.nmsImplementations.XMaterial;
 import io.github.retrooper.packetevents.utils.player.ClientVersion;
+import org.bukkit.Material;
 import org.bukkit.block.BlockFace;
 
+import java.util.HashSet;
+import java.util.Set;
+
+// 1.13 clients on 1.12 servers have this mostly working, but chorus flowers don’t attach to chorus plants.
+// 1.12 clients run their usual calculations on 1.13 servers, same as 1.12 servers
+// 1.13 clients on 1.13 servers get everything included in the block data, no world reading required
 public class DynamicChorusPlant implements CollisionFactory {
     private static final BlockFace[] directions = new BlockFace[]{BlockFace.NORTH, BlockFace.EAST, BlockFace.SOUTH, BlockFace.WEST, BlockFace.UP, BlockFace.DOWN};
     private static final CollisionBox[] modernShapes = makeShapes();
+    private static final Material END_STONE = XMaterial.END_STONE.parseMaterial();
+    private static final Material CHORUS_FLOWER = XMaterial.CHORUS_FLOWER.parseMaterial();
+    private static final Material CHORUS_PLANT = XMaterial.CHORUS_PLANT.parseMaterial();
 
     private static CollisionBox[] makeShapes() {
         float f = 0.5F - (float) 0.3125;
@@ -44,20 +55,84 @@ public class DynamicChorusPlant implements CollisionFactory {
 
     @Override
     public CollisionBox fetch(GrimPlayer player, ClientVersion version, WrappedBlockDataValue block, int x, int y, int z) {
-        WrappedMultipleFacing facing = (WrappedMultipleFacing) block;
+        // Player is 1.12- on 1.13 server
+        // Player is 1.12 on 1.12 server
+        if (version.isOlderThanOrEquals(ClientVersion.v_1_12_2)) {
+            return getLegacyBoundingBox(player, version, x, y, z);
+        }
 
-        return modernShapes[getAABBIndex(facing)].copy();
+        Set<BlockFace> directions;
+
+        if (XMaterial.isNewVersion()) {
+            // Player is 1.13 on 1.13 server
+            directions = ((WrappedMultipleFacing) block).getDirections();
+        } else {
+            // Player is 1.13 on 1.12 server
+            directions = getLegacyStates(player, version, x, y, z);
+        }
+
+        // Player is 1.13+ on 1.13+ server
+        return modernShapes[getAABBIndex(directions)].copy();
     }
 
-    protected int getAABBIndex(WrappedMultipleFacing p_196486_1_) {
+    public CollisionBox getLegacyBoundingBox(GrimPlayer player, ClientVersion version, int x, int y, int z) {
+        Set<BlockFace> faces = getLegacyStates(player, version, x, y, z);
+
+        float f1 = faces.contains(BlockFace.WEST) ? 0.0F : 0.1875F;
+        float f2 = faces.contains(BlockFace.DOWN) ? 0.0F : 0.1875F;
+        float f3 = faces.contains(BlockFace.NORTH) ? 0.0F : 0.1875F;
+        float f4 = faces.contains(BlockFace.EAST) ? 1.0F : 0.8125F;
+        float f5 = faces.contains(BlockFace.UP) ? 1.0F : 0.8125F;
+        float f6 = faces.contains(BlockFace.SOUTH) ? 1.0F : 0.8125F;
+
+        return new SimpleCollisionBox(f1, f2, f3, f4, f5, f6);
+    }
+
+    protected int getAABBIndex(Set<BlockFace> p_196486_1_) {
         int i = 0;
 
         for (int j = 0; j < directions.length; ++j) {
-            if (p_196486_1_.getDirections().contains(directions[j])) {
+            if (p_196486_1_.contains(directions[j])) {
                 i |= 1 << j;
             }
         }
 
         return i;
+    }
+
+    public Set<BlockFace> getLegacyStates(GrimPlayer player, ClientVersion version, int x, int y, int z) {
+        Set<BlockFace> faces = new HashSet<>();
+
+        // 1.13 clients on 1.12 servers don't see chorus flowers attached to chorus because of a ViaVersion bug
+        Material versionFlower = version.isOlderThanOrEquals(ClientVersion.v_1_12_2) ? CHORUS_FLOWER : null;
+
+        Material downBlock = player.compensatedWorld.getBukkitMaterialAt(x, y - 1, z);
+        Material upBlock = player.compensatedWorld.getBukkitMaterialAt(x, y + 1, z);
+        Material northBlock = player.compensatedWorld.getBukkitMaterialAt(x, y, z - 1);
+        Material eastBlock = player.compensatedWorld.getBukkitMaterialAt(x + 1, y, z);
+        Material southBlock = player.compensatedWorld.getBukkitMaterialAt(x, y, z + 1);
+        Material westBlock = player.compensatedWorld.getBukkitMaterialAt(x - 1, y, z);
+
+        if (downBlock == CHORUS_PLANT || downBlock == versionFlower || downBlock == END_STONE) {
+            faces.add(BlockFace.DOWN);
+        }
+
+        if (upBlock == CHORUS_PLANT || upBlock == versionFlower) {
+            faces.add(BlockFace.UP);
+        }
+        if (northBlock == CHORUS_PLANT || northBlock == versionFlower) {
+            faces.add(BlockFace.EAST);
+        }
+        if (eastBlock == CHORUS_PLANT || eastBlock == versionFlower) {
+            faces.add(BlockFace.EAST);
+        }
+        if (southBlock == CHORUS_PLANT || southBlock == versionFlower) {
+            faces.add(BlockFace.NORTH);
+        }
+        if (westBlock == CHORUS_PLANT || westBlock == versionFlower) {
+            faces.add(BlockFace.NORTH);
+        }
+
+        return faces;
     }
 }
