1f989a9c0fa6931e88955f117e433969828afe3e
DefineOutside <49797025+MWHunter@users.noreply.github.com>
2021-07-21 14:55:23 -0500

    Support and protect owners from ViaBackwards stupidity

--- src/main/java/ac/grim/grimac/GrimAC.java
+++ src/main/java/ac/grim/grimac/GrimAC.java
@@ -11,6 +11,7 @@ import io.github.retrooper.packetevents.PacketEvents;
 import io.github.retrooper.packetevents.settings.PacketEventsSettings;
 import io.github.retrooper.packetevents.utils.server.ServerVersion;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -98,6 +99,32 @@ public final class GrimAC extends JavaPlugin {
                 player.sendTransactionOrPingPong();
             }
         }, 1, 1);
+
+        if (ServerVersion.getVersion().isNewerThanOrEquals(ServerVersion.v_1_17)) {
+            // Enable ping -> transaction packet
+            System.setProperty("com.viaversion.handlePingsAsInvAcknowledgements", "true");
+
+            // Check if we support this property
+            try {
+                Plugin viaBackwards = Bukkit.getPluginManager().getPlugin("ViaBackwards");
+                if (viaBackwards != null) {
+                    String[] split = viaBackwards.getDescription().getVersion().replace("-SNAPSHOT", "").split("\\.");
+
+                    if (split.length == 3) {
+                        // If the version is before 4.0.2
+                        if (Integer.parseInt(split[0]) < 4 || (Integer.parseInt(split[1]) == 0 && Integer.parseInt(split[2]) < 2)) {
+                            getLogger().warning(ChatColor.RED + "Please update ViaBackwards to 4.0.2 or newer");
+                            getLogger().warning(ChatColor.RED + "An important packet is broken for 1.16 and below clients on this ViaBackwards version");
+                            getLogger().warning(ChatColor.RED + "Disabling all checks for 1.16 and below players as otherwise they WILL be falsely banned");
+                            getLogger().warning(ChatColor.RED + "Supported version: " + ChatColor.WHITE + "https://github.com/ViaVersion/ViaBackwards/actions/runs/1039987269");
+
+                            PlayerJoinQuitListener.isViaLegacyUpdated = false;
+                        }
+                    }
+                }
+            } catch (Exception ignored) {
+            }
+        }
     }
 
     public void registerEvents() {
