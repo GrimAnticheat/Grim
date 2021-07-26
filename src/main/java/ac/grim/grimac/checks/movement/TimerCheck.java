4763b31eb23c39b5c2b4d36bf5d8ca2d3b48c0f4
DefineOutside <49797025+MWHunter@users.noreply.github.com>
2021-07-26 14:01:41 -0500

    Refactor timer check removing all magic values

--- src/main/java/ac/grim/grimac/checks/movement/TimerCheck.java
+++ src/main/java/ac/grim/grimac/checks/movement/TimerCheck.java
@@ -2,6 +2,7 @@ package ac.grim.grimac.checks.movement;
 
 import ac.grim.grimac.checks.Check;
 import ac.grim.grimac.player.GrimPlayer;
+import io.github.retrooper.packetevents.utils.pair.Pair;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 
@@ -14,11 +15,10 @@ public class TimerCheck extends Check {
     long timerTransaction = 0;
 
     // To patch out lag spikes
-    long lastLagSpike = 0;
-    long beginningLagSpikeReceivedRealtime = 0;
-    long transactionsReceivedAtEndOfLastCheck = 0;
+    long timeSinceLastTransaction = 0;
+    long transactionsReceivedAtEndOfLastCheck = System.currentTimeMillis();
 
-    ConcurrentLinkedQueue<Integer> trackedTransactions = new ConcurrentLinkedQueue<>();
+    ConcurrentLinkedQueue<Pair<Long, Long>> lagSpikeToTransactionFloor = new ConcurrentLinkedQueue<>();
 
     // Proof for this timer check
     // https://i.imgur.com/Hk2Wb6c.png
@@ -27,14 +27,16 @@ public class TimerCheck extends Check {
     // Proof lies that client -> server ping will always be lower
     //
     // The largest gap is the floor for movements
-    // We increment this by 1 every time we get a movement
     // If the smaller gap surpasses the larger gap, the player is cheating
     //
     // This usually flags 1.01 on low ping extremely quickly
-    // Higher ping scales proportionately, and will flag less quickly but still can flag 1.01
+    // Higher ping/low fps scales proportionately, and will flag less quickly but will still always flag 1.01
+    // Players standing still will reset this amount of time
     //
     // This is better than traditional timer checks because ping fluctuations will never affect this check
     // As we are tying this check to the player's ping, rather than real time.
+    //
+    // Tested 10/20/30 fps and f3 + t spamming for lag spikes at 0 ping localhost/200 ping clumsy, no falses
     public TimerCheck(GrimPlayer player) {
         this.player = player;
     }
@@ -50,7 +52,7 @@ public class TimerCheck extends Check {
         timerTransaction += 50;
 
         if (timerTransaction > System.currentTimeMillis()) {
-            Bukkit.broadcastMessage(ChatColor.RED + "THE PLAYER HAS TIMER! (Check stable as of 7/25/21, report if not timer!)");
+            Bukkit.broadcastMessage(ChatColor.RED + "THE PLAYER HAS TIMER! (Can't false as of 7/26/21, open github issue if not timer!)");
             // Reset the violation by 1 movement
             timerTransaction -= 50;
         }
@@ -60,16 +62,30 @@ public class TimerCheck extends Check {
         Bukkit.broadcastMessage("Received: " + (System.currentTimeMillis() - player.getPlayerClockAtLeast()));
         Bukkit.broadcastMessage("==================");*/
 
-        // Detect lag spikes of minimum 130 ms (missing 2 transactions missing)
-        if (System.currentTimeMillis() - transactionsReceivedAtEndOfLastCheck > 130) {
-            // Stop players from spamming lag spikes to become exempt
-            // Spamming F3 + T, I can still flag 1.07 timer
-            // Probably can still flag lower over more time, if the client is spamming fake lag spikes
-            timerTransaction = Math.max(timerTransaction, beginningLagSpikeReceivedRealtime);
-            beginningLagSpikeReceivedRealtime = transactionsReceivedAtEndOfLastCheck;
-            lastLagSpike = System.currentTimeMillis();
-        } else if (System.currentTimeMillis() - lastLagSpike > 1000) {
-            timerTransaction = Math.max(timerTransaction, player.getPlayerClockAtLeast());
+        // Calculate time since last transaction - affected by 50 ms delay movement packets and
+        timeSinceLastTransaction = System.currentTimeMillis() + (System.currentTimeMillis() - transactionsReceivedAtEndOfLastCheck);
+        // As we don't check players standing still, cap this at 1000 ms
+        // A second is more than enough time for all packets from the lag spike to arrive
+        // Exempting over a 30 second lag spike will lead to bypasses where the player can catch up movement
+        // packets that were lost by standing still
+        timeSinceLastTransaction = Math.min(timeSinceLastTransaction, System.currentTimeMillis() + 1000);
+
+        // Add this into a queue so that new lag spikes do not override previous lag spikes
+        lagSpikeToTransactionFloor.add(new Pair<>(timeSinceLastTransaction, transactionsReceivedAtEndOfLastCheck));
+
+        // Find the safe floor, lag spikes affect transactions, which is bad.
+        Pair<Long, Long> lagSpikePair = lagSpikeToTransactionFloor.peek();
+        if (lagSpikePair != null) {
+            do {
+                if (System.currentTimeMillis() > lagSpikePair.getFirst()) {
+                    timerTransaction = Math.max(timerTransaction, lagSpikePair.getSecond());
+
+                    lagSpikeToTransactionFloor.poll();
+                    lagSpikePair = lagSpikeToTransactionFloor.peek();
+                } else {
+                    break;
+                }
+            } while (lagSpikePair != null);
         }
 
         transactionsReceivedAtEndOfLastCheck = player.getPlayerClockAtLeast();
