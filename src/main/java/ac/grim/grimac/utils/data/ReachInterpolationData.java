320dcdbb2bbda3c07eef8dfa480eb7ebea5a2780
DefineOutside <49797025+MWHunter@users.noreply.github.com>
2021-07-28 10:02:03 -0500

    Add infallible copyright note to 3.01 reach check

--- src/main/java/ac/grim/grimac/utils/data/ReachInterpolationData.java
+++ src/main/java/ac/grim/grimac/utils/data/ReachInterpolationData.java
@@ -1,3 +1,18 @@
+// This file was designed and is an original check for GrimAC
+// Copyright (C) 2021 DefineOutside
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 package ac.grim.grimac.utils.data;
 
 import ac.grim.grimac.utils.collisions.datatypes.SimpleCollisionBox;
@@ -5,6 +20,7 @@ import ac.grim.grimac.utils.nmsImplementations.GetBoundingBox;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 
+// You may not copy the check unless you are licensed under GPL
 public class ReachInterpolationData {
     public final SimpleCollisionBox targetLocation;
     public SimpleCollisionBox startingLocation;
--- src/main/java/ac/grim/grimac/utils/data/packetentity/PlayerReachEntity.java
+++ src/main/java/ac/grim/grimac/utils/data/packetentity/PlayerReachEntity.java
@@ -1,3 +1,18 @@
+// This file was designed and is an original check for GrimAC
+// Copyright (C) 2021 DefineOutside
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 package ac.grim.grimac.utils.data.packetentity;
 
 import ac.grim.grimac.utils.collisions.datatypes.SimpleCollisionBox;
@@ -7,6 +22,7 @@ import io.github.retrooper.packetevents.utils.vector.Vector3d;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 
+// You may not copy this check unless your anticheat is licensed under GPL
 public class PlayerReachEntity {
     public Vector3d serverPos;
     public ReachInterpolationData oldPacketLocation;
