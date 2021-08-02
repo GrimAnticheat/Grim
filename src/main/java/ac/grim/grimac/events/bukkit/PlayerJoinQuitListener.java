146f037ed92839ff5ad47a07832c68d02f56e9bf
DefineOutside <49797025+MWHunter@users.noreply.github.com>
2021-08-02 15:23:26 -0500

    Temporary hack for 1.12- underwater flying

--- src/main/java/ac/grim/grimac/events/bukkit/PlayerJoinQuitListener.java
+++ src/main/java/ac/grim/grimac/events/bukkit/PlayerJoinQuitListener.java
@@ -57,6 +57,7 @@ public class PlayerJoinQuitListener implements Listener {
         player.uncertaintyHandler.pistonPushing.add(0d);
         player.uncertaintyHandler.collidingEntities.add(0);
         player.uncertaintyHandler.flyingStatusSwitchHack.add(false);
+        player.uncertaintyHandler.legacyUnderwaterFlyingHack.add(false);
         player.uncertaintyHandler.stuckMultiplierZeroPointZeroThree.add(false);
         player.uncertaintyHandler.boatCollision.add(false);
 
