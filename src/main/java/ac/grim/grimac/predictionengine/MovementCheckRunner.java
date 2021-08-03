89691c92099a8e25a04bf8b6885512d68d1e9159
DefineOutside <49797025+MWHunter@users.noreply.github.com>
2021-08-03 15:30:36 -0500

    Brute force attack slowing in the correct spot

--- src/main/java/ac/grim/grimac/predictionengine/MovementCheckRunner.java
+++ src/main/java/ac/grim/grimac/predictionengine/MovementCheckRunner.java
@@ -542,12 +542,14 @@ public class MovementCheckRunner {
         player.trigHandler.setOffset(offset);
         player.compensatedRiptide.handleRemoveRiptide();
 
-        player.bukkitPlayer.sendMessage("P: " + color + player.predictedVelocity.vector.getX() + " " + player.predictedVelocity.vector.getY() + " " + player.predictedVelocity.vector.getZ());
-        player.bukkitPlayer.sendMessage("A: " + color + player.actualMovement.getX() + " " + player.actualMovement.getY() + " " + player.actualMovement.getZ());
-        player.bukkitPlayer.sendMessage("O: " + color + offset + " " + player.uncertaintyHandler.lastTeleportTicks);
+        if (color == ChatColor.YELLOW || color == ChatColor.RED) {
+            player.bukkitPlayer.sendMessage("P: " + color + player.predictedVelocity.vector.getX() + " " + player.predictedVelocity.vector.getY() + " " + player.predictedVelocity.vector.getZ());
+            player.bukkitPlayer.sendMessage("A: " + color + player.actualMovement.getX() + " " + player.actualMovement.getY() + " " + player.actualMovement.getZ());
+            player.bukkitPlayer.sendMessage("O: " + color + offset);
+        }
 
         GrimAC.staticGetLogger().info(player.bukkitPlayer.getName() + " P: " + color + player.predictedVelocity.vector.getX() + " " + player.predictedVelocity.vector.getY() + " " + player.predictedVelocity.vector.getZ());
         GrimAC.staticGetLogger().info(player.bukkitPlayer.getName() + " A: " + color + player.actualMovement.getX() + " " + player.actualMovement.getY() + " " + player.actualMovement.getZ());
-        GrimAC.staticGetLogger().info(player.bukkitPlayer.getName() + " O: " + color + offset + " " + player.uncertaintyHandler.lastTeleportTicks);
+        GrimAC.staticGetLogger().info(player.bukkitPlayer.getName() + " O: " + color + offset);
     }
 }
