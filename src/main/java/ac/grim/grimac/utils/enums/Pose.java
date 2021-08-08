defcf784d153e4c20006dd93204f475651cba12b
DefineOutside <49797025+MWHunter@users.noreply.github.com>
2021-08-07 20:01:59 -0500

    Fix crouching hitbox for 1.9-1.13 players

--- src/main/java/ac/grim/grimac/utils/enums/Pose.java
+++ src/main/java/ac/grim/grimac/utils/enums/Pose.java
@@ -6,6 +6,7 @@ public enum Pose {
     SLEEPING(0.2f, 0.2f),
     SWIMMING(0.6f, 0.6f),
     SPIN_ATTACK(0.6f, 0.6f),
+    NINE_CROUCHING(0.6f, 1.65f), // 1.9-1.13 clients have a slightly different crouching hitbox
     CROUCHING(0.6f, 1.5f),
     DYING(0.2f, 0.2f),
     LONG_JUMPING(0.6f, 1.8f); // DUMMY (players can't have this pose)
