13f71ad0d76767572e904b9758912febeff9951b
DefineOutside <49797025+MWHunter@users.noreply.github.com>
2021-08-19 18:44:17 -0500

    Fix rare temporary desync when placing slabs on top of one another in the same tick

--- src/main/java/ac/grim/grimac/utils/data/BasePlayerChangeBlockData.java
+++ src/main/java/ac/grim/grimac/utils/data/BasePlayerChangeBlockData.java
@@ -1,27 +1,31 @@
 package ac.grim.grimac.utils.data;
 
-import com.google.common.base.Objects;
 import lombok.Data;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Data
 public abstract class BasePlayerChangeBlockData {
+    private static final AtomicInteger id = new AtomicInteger(0);
     public final int transaction;
     public final int blockX;
     public final int blockY;
     public final int blockZ;
+    public final int uniqueID;
 
     public BasePlayerChangeBlockData(int transaction, int blockX, int blockY, int blockZ) {
         this.transaction = transaction;
         this.blockX = blockX;
         this.blockY = blockY;
         this.blockZ = blockZ;
+        this.uniqueID = id.getAndIncrement();
     }
 
     public abstract int getCombinedID();
 
     @Override
     public int hashCode() {
-        return Objects.hashCode(transaction, blockX, blockY, blockZ);
+        return uniqueID;
     }
 
     @Override
@@ -29,6 +33,6 @@ public abstract class BasePlayerChangeBlockData {
         if (this == o) return true;
         if (!(o instanceof BasePlayerChangeBlockData)) return false;
         BasePlayerChangeBlockData that = (BasePlayerChangeBlockData) o;
-        return transaction == that.transaction && blockX == that.blockX && blockY == that.blockY && blockZ == that.blockZ;
+        return transaction == that.transaction && blockX == that.blockX && blockY == that.blockY && blockZ == that.blockZ && uniqueID == that.uniqueID;
     }
 }
