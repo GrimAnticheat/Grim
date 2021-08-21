5c968182dc9bf4afec28f4416d6d6cdd676776ca
DefineOutside <49797025+MWHunter@users.noreply.github.com>
2021-08-21 09:25:28 -0500

    1.13 swimming fluid falling

--- src/main/java/ac/grim/grimac/utils/nmsImplementations/FluidFallingAdjustedMovement.java
+++ src/main/java/ac/grim/grimac/utils/nmsImplementations/FluidFallingAdjustedMovement.java
@@ -1,12 +1,14 @@
 package ac.grim.grimac.utils.nmsImplementations;
 
 import ac.grim.grimac.player.GrimPlayer;
+import io.github.retrooper.packetevents.utils.player.ClientVersion;
 import org.bukkit.util.Vector;
 
 public class FluidFallingAdjustedMovement {
     public static Vector getFluidFallingAdjustedMovement(GrimPlayer player, double d, boolean bl, Vector vec3) {
         if (player.hasGravity && !player.isSprinting) {
-            double d2 = bl && Math.abs(vec3.getY() - 0.005) >= 0.003 && Math.abs(vec3.getY() - d / 16.0) < 0.003 ? -0.003 : vec3.getY() - d / 16.0;
+            boolean falling = player.getClientVersion().isNewerThanOrEquals(ClientVersion.v_1_14) ? bl : vec3.getY() < 0;
+            double d2 = falling && Math.abs(vec3.getY() - 0.005) >= 0.003 && Math.abs(vec3.getY() - d / 16.0) < 0.003 ? -0.003 : vec3.getY() - d / 16.0;
             return new Vector(vec3.getX(), d2, vec3.getZ());
         }
         return vec3;
