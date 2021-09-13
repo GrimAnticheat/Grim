74eaf9733041e67ca404e60b33228aee0559fa9e
DefineOutside <49797025+MWHunter@users.noreply.github.com>
2021-09-13 17:52:16 -0500

    Allow the console to send commands

--- src/main/java/ac/grim/grimac/commands/GrimPerf.java
+++ src/main/java/ac/grim/grimac/commands/GrimPerf.java
@@ -6,20 +6,20 @@ import co.aikar.commands.BaseCommand;
 import co.aikar.commands.annotation.CommandAlias;
 import co.aikar.commands.annotation.CommandPermission;
 import co.aikar.commands.annotation.Subcommand;
-import org.bukkit.entity.Player;
+import org.bukkit.command.CommandSender;
 
 @CommandAlias("grim|grimac")
 public class GrimPerf extends BaseCommand {
     @Subcommand("perf|performance")
     @CommandPermission("grim.performance")
-    public void onPerformance(Player player) {
+    public void onPerformance(CommandSender sender) {
         double nano = MovementCheckRunner.executor.getLongComputeTime() * 20 * GrimAPI.INSTANCE.getPlayerDataManager().size();
         // Convert this into seconds
         double seconds = nano / 1e9;
 
-        player.sendMessage("Nanoseconds per prediction: " + MovementCheckRunner.executor.getComputeTime());
-        player.sendMessage("Estimated load (threads): " + seconds);
-        player.sendMessage("Prediction threads: " + MovementCheckRunner.executor.getPoolSize());
-        player.sendMessage("Players online: " + GrimAPI.INSTANCE.getPlayerDataManager().size());
+        sender.sendMessage("Nanoseconds per prediction: " + MovementCheckRunner.executor.getComputeTime());
+        sender.sendMessage("Estimated load (threads): " + seconds);
+        sender.sendMessage("Prediction threads: " + MovementCheckRunner.executor.getPoolSize());
+        sender.sendMessage("Players online: " + GrimAPI.INSTANCE.getPlayerDataManager().size());
     }
 }
