e356bc5354767a6f1dbf9f1516102586092c2787
VytskaLT <VytskaLT@protonmail.com>
2021-12-17 15:39:08 +0200

    Make GrimMath use UtilityClass

--- src/main/java/ac/grim/grimac/utils/math/GrimMath.java
+++ src/main/java/ac/grim/grimac/utils/math/GrimMath.java
@@ -1,24 +1,28 @@
 package ac.grim.grimac.utils.math;
 
+import lombok.experimental.UtilityClass;
+
 import java.util.List;
 
+@UtilityClass
 public class GrimMath {
-    public static int floor(double d) {
+
+    public int floor(double d) {
         return (int) Math.floor(d);
     }
 
-    public static int ceil(double d) {
+    public int ceil(double d) {
         return (int) Math.ceil(d);
     }
 
-    public static double clamp(double d, double d2, double d3) {
+    public double clamp(double d, double d2, double d3) {
         if (d < d2) {
             return d2;
         }
         return Math.min(d, d3);
     }
 
-    public static float clampFloat(float d, float d2, float d3) {
+    public float clampFloat(float d, float d2, float d3) {
         if (d < d2) {
             return d2;
         }
@@ -35,15 +39,15 @@ public class GrimMath {
     // Distance to (1 / 64) when we are around 0.1
     //
     // Someone should likely just refactor this entire method, although it is cold being called twice every movement
-    public static double distanceToHorizontalCollision(double position) {
+    public double distanceToHorizontalCollision(double position) {
         return Math.min(Math.abs(position % (1 / 640d)), Math.abs(Math.abs(position % (1 / 640d)) - (1 / 640d)));
     }
 
-    public static boolean equal(double d, double d2) {
+    public boolean equal(double d, double d2) {
         return Math.abs(d2 - d) < 9.999999747378752E-6;
     }
 
-    public static double calculateAverage(List<Integer> marks) {
+    public double calculateAverage(List<Integer> marks) {
         long sum = 0;
         for (Integer mark : marks) {
             sum += mark;
@@ -51,7 +55,7 @@ public class GrimMath {
         return marks.isEmpty() ? 0 : 1.0 * sum / marks.size();
     }
 
-    public static double calculateAverageLong(List<Long> marks) {
+    public double calculateAverageLong(List<Long> marks) {
         long sum = 0;
         for (Long mark : marks) {
             sum += mark;
