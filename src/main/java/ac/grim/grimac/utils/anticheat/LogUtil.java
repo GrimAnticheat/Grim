9dffbc0b98304b31fb5e20b06f0c1bf8eb509b28
MWHunter <s0521458@student.rockvalleycollege.edu>
2022-01-26 22:10:33 -0600

    Add suggested changes
    Co-authored-by: VytskaLT <VytskaLT@protonmail.com>

--- src/main/java/ac/grim/grimac/utils/anticheat/LogUtil.java
+++ src/main/java/ac/grim/grimac/utils/anticheat/LogUtil.java
@@ -3,17 +3,23 @@ package ac.grim.grimac.utils.anticheat;
 import ac.grim.grimac.GrimAPI;
 import lombok.experimental.UtilityClass;
 
+import java.util.logging.Logger;
+
 @UtilityClass
 public class LogUtil {
     public void info(final String info) {
-        GrimAPI.INSTANCE.getPlugin().getLogger().info(info);
+        getLogger().info(info);
     }
 
     public void warn(final String warn) {
-        GrimAPI.INSTANCE.getPlugin().getLogger().info(warn);
+        getLogger().info(warn);
     }
 
     public void error(final String error) {
-        GrimAPI.INSTANCE.getPlugin().getLogger().info(error);
+        getLogger().info(error);
+    }
+
+    public Logger getLogger() {
+        return GrimAPI.INSTANCE.getPlugin().getLogger();
     }
 }
