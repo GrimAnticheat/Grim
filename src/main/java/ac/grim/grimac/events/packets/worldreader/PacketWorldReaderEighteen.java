e3d5a01467f1e80e99ee1939452944abdb7f73a9
MWHunter <s0521458@student.rockvalleycollege.edu>
2022-02-18 11:00:04 -0600

    Save memory usage with 1.18 chunks

--- src/main/java/ac/grim/grimac/events/packets/worldreader/PacketWorldReaderEighteen.java
+++ src/main/java/ac/grim/grimac/events/packets/worldreader/PacketWorldReaderEighteen.java
@@ -4,6 +4,7 @@ import ac.grim.grimac.player.GrimPlayer;
 import com.github.retrooper.packetevents.event.PacketSendEvent;
 import com.github.retrooper.packetevents.protocol.stream.NetStreamInput;
 import com.github.retrooper.packetevents.protocol.world.chunk.BaseChunk;
+import com.github.retrooper.packetevents.protocol.world.chunk.impl.v_1_18.Chunk_v1_18;
 import com.github.retrooper.packetevents.protocol.world.chunk.reader.impl.ChunkReader_v1_18;
 import com.github.retrooper.packetevents.wrapper.PacketWrapper;
 
@@ -22,6 +23,15 @@ public class PacketWorldReaderEighteen extends BasePacketWorldReader {
         wrapper.readNBT();
 
         BaseChunk[] chunks = new ChunkReader_v1_18().read(null, null, true, false, false, event.getUser().getTotalWorldHeight() >> 4, null, new NetStreamInput(new ByteArrayInputStream(wrapper.readByteArray())));
+
+        // Remove biomes to save memory
+        for (int i = 0; i < chunks.length; i++) {
+            Chunk_v1_18 chunk = (Chunk_v1_18) chunks[i];
+            if (chunk != null) {
+                // I know I'm passing null into @NotNull, but it shouldn't affect anything.
+                chunks[i] = new Chunk_v1_18(chunk.getBlockCount(), chunk.getChunkData(), null);
+            }
+        }
 
         addChunkToCache(player, chunks, true, x, z);
 
