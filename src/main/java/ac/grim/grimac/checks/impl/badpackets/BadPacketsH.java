61b283ab1e1e3e18ae9bca1328edb5327c7dfb91
LewUwU <72635645+LewUwU@users.noreply.github.com>
2022-03-06 21:21:55 -0400

    Improved code (Thanks @amnoah)

--- src/main/java/ac/grim/grimac/checks/impl/badpackets/BadPacketsH.java
+++ src/main/java/ac/grim/grimac/checks/impl/badpackets/BadPacketsH.java
@@ -3,12 +3,10 @@ package ac.grim.grimac.checks.impl.badpackets;
 import ac.grim.grimac.checks.CheckData;
 import ac.grim.grimac.checks.type.PacketCheck;
 import ac.grim.grimac.player.GrimPlayer;
-import io.github.retrooper.packetevents.PacketEvents;
-import io.github.retrooper.packetevents.utils.server.ServerVersion;
-import io.github.retrooper.packetevents.utils.player.ClientVersion;
 import com.github.retrooper.packetevents.event.PacketReceiveEvent;
 import com.github.retrooper.packetevents.protocol.packettype.PacketType;
 import com.github.retrooper.packetevents.wrapper.play.client.WrappedPacketInUseEntity;
+import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientPlayerFlying;
 
 @CheckData(name = "BadPacketsH")
 public class BadPacketsH extends PacketCheck {
@@ -23,20 +21,16 @@ public class BadPacketsH extends PacketCheck {
         if (event.getPacketType() == PacketType.Play.Client.USE_ENTITY) {
             WrappedPacketInUseEntity packet = new WrappedPacketInUseEntity(event);
 
-            boolean exempt = PacketEvents.getAPI().getServerManager().getVersion().isNewerThanOrEquals(ServerVersion.V_1_9) || 
-			                 player.getClientVersion().isNewerThanOrEquals(ClientVersion.V_1_9);
-
-            if (packet.getAction() != WrappedPacketInUseEntity.EntityUseAction.ATTACK || exempt || swung) return;
+            if (packet.getAction() != WrappedPacketInUseEntity.EntityUseAction.ATTACK) return;
 			
-			          flagAndAlert();
-        } else if (event.getPacketType() == PacketType.Play.Client.ANIMATION) {
-			      swung = true;
-		    } else if (event.getPacketType() == PacketType.Play.Client.PLAYER_FLYING || 
-		        event.getPacketType() == PacketType.Play.Client.PLAYER_POSITION || 
-				    event.getPacketType() == PacketType.Play.Client.PLAYER_POSITION_AND_ROTATION || 
-				    event.getPacketType() == PacketType.Play.Client.PLAYER_ROTATION) {
-			      swung = false;
-		    }
+	    if (!swung) flagAndAlert();
+            swung = false;
+	} else if (event.getPacketType() == PacketType.Play.Client.ANIMATION) {
+	    swung = true;
+	} else if (WrapperPlayClientPlayerFlying.isFlying(event.getPacket()) {
+	    swung = false;
+	}
+      }
     }
 }
-
+		   
