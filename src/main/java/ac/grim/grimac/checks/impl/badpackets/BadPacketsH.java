a065c5057694725c40f00b56c11e0ffb9a348878
LewUwU <72635645+LewUwU@users.noreply.github.com>
2022-03-07 06:34:11 -0400

    Update src/main/java/ac/grim/grimac/checks/impl/badpackets/BadPacketsH.java
    Co-authored-by: Jaren <Jaren@Knodel.com>

--- src/main/java/ac/grim/grimac/checks/impl/badpackets/BadPacketsH.java
+++ src/main/java/ac/grim/grimac/checks/impl/badpackets/BadPacketsH.java
@@ -5,7 +5,7 @@ import ac.grim.grimac.checks.type.PacketCheck;
 import ac.grim.grimac.player.GrimPlayer;
 import com.github.retrooper.packetevents.event.PacketReceiveEvent;
 import com.github.retrooper.packetevents.protocol.packettype.PacketType;
-import com.github.retrooper.packetevents.wrapper.play.client.WrappedPacketInUseEntity;
+import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientInteractEntity;
 import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientPlayerFlying;
 
 @CheckData(name = "BadPacketsH")
