2f2450ac2178bc90c4f8f94e92e644d62a712b8b
MWHunter <s0521458@student.rockvalleycollege.edu>
2022-03-06 20:51:48 -0600

    Re-enable bstats

--- src/main/java/ac/grim/grimac/manager/init/start/BStats.java
+++ src/main/java/ac/grim/grimac/manager/init/start/BStats.java
@@ -1,11 +1,13 @@
 package ac.grim.grimac.manager.init.start;
 
+import ac.grim.grimac.GrimAPI;
 import ac.grim.grimac.manager.init.Initable;
+import org.bstats.bukkit.Metrics;
 
 public class BStats implements Initable {
     @Override
     public void start() {
         int pluginId = 12820; // <-- Replace with the id of your plugin!
-        //Metrics metrics = new Metrics(GrimAPI.INSTANCE.getPlugin(), pluginId);
+        Metrics metrics = new Metrics(GrimAPI.INSTANCE.getPlugin(), pluginId);
     }
 }
