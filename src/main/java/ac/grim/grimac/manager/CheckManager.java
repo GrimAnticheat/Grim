492635624bf47ec90c763f2c710d08c25c77ac61
LewUwU <72635645+LewUwU@users.noreply.github.com>
2022-03-06 23:03:59 -0400

    Register KillAuraA

--- src/main/java/ac/grim/grimac/manager/CheckManager.java
+++ src/main/java/ac/grim/grimac/manager/CheckManager.java
@@ -5,6 +5,7 @@ import ac.grim.grimac.checks.impl.aim.processor.AimProcessor;
 import ac.grim.grimac.checks.impl.aim.processor.Cinematic;
 import ac.grim.grimac.checks.impl.badpackets.*;
 import ac.grim.grimac.checks.impl.combat.Reach;
+import ac.grim.grimac.checks.impl.combat.killaura.KillAuraA;
 import ac.grim.grimac.checks.impl.crash.CrashA;
 import ac.grim.grimac.checks.impl.crash.CrashB;
 import ac.grim.grimac.checks.impl.disabler.DisablerA;
@@ -52,6 +53,7 @@ public class CheckManager {
     public CheckManager(GrimPlayer player) {
         // Include post checks in the packet check too
         packetChecks = new ImmutableClassToInstanceMap.Builder<PacketCheck>()
+                .put(KillAuraA.class, new KillAuraA(player))
                 .put(Reach.class, new Reach(player))
                 .put(PacketEntityReplication.class, new PacketEntityReplication(player))
                 .put(PacketChangeGameState.class, new PacketChangeGameState(player))
