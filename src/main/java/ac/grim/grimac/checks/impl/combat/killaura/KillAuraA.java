1edc51ce83c69638745c6c52e256d2ff99310a6c
LewUwU <72635645+LewUwU@users.noreply.github.com>
2022-03-07 06:43:08 -0400

    Update KillAuraA.java

--- src/main/java/ac/grim/grimac/checks/impl/combat/killaura/KillAuraA.java
+++ src/main/java/ac/grim/grimac/checks/impl/combat/killaura/KillAuraA.java
@@ -5,7 +5,7 @@ import ac.grim.grimac.checks.type.PacketCheck;
 import ac.grim.grimac.player.GrimPlayer;
 import com.github.retrooper.packetevents.event.PacketReceiveEvent;
 import com.github.retrooper.packetevents.protocol.packettype.PacketType;
-import com.github.retrooper.packetevents.wrapper.play.client.WrappedPacketInUseEntity;
+import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientInteractEntity;
 import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientPlayerFlying;
 
 /**
@@ -17,26 +17,26 @@ import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientPl
 @CheckData(name = "KillAuraA")
 public class KillAuraA extends PacketCheck {
     private int ticks, lastEntityId;
-  
+
     public KillAuraA(GrimPlayer player) {
         super(player);
     }
 
     @Override
     public void onPacketReceive(PacketReceiveEvent event) {
-        if (event.getPacketType() == PacketType.Play.Client.USE_ENTITY) {
-            WrappedPacketInUseEntity packet = new WrappedPacketInUseEntity(event);
-          
-            if (packet.getAction() != WrappedPacketInUseEntity.EntityUseAction.ATTACK) return;
-          
+        if (event.getPacketType() == PacketType.Play.Client.INTERACT_ENTITY) {
+            WrapperPlayClientInteractEntity packet = new WrapperPlayClientInteractEntity(event);
+
+            if (packet.getAction() != WrapperPlayClientInteractEntity.InteractAction.ATTACK) return;
+
             if (packet.getEntityId() != lastEntityId) {
-              if (++ticks > 1) {
-                flagAndAlert();
-              }
+                if (++ticks > 1) {
+                    flagAndAlert();
+                }
             }
-          
-            lastEntityId = wrapper.getEntityId();
-        } else if (WrapperPlayClientPlayerFlying.isFlying(event.getPacket())) {
+
+            lastEntityId = packet.getEntityId();
+        } else if (WrapperPlayClientPlayerFlying.isFlying(event.getPacketType())) {
             ticks = 0;
         }
     }
