fa1b824e3eecda282a51e03d957202c4a74479f8
LewUwU <72635645+LewUwU@users.noreply.github.com>
2022-03-06 21:58:45 -0400

    Fix

--- src/main/java/ac/grim/grimac/checks/impl/disabler/DisablerE.java
+++ src/main/java/ac/grim/grimac/checks/impl/disabler/DisablerE.java
@@ -5,7 +5,7 @@ import ac.grim.grimac.checks.type.PacketCheck;
 import ac.grim.grimac.player.GrimPlayer;
 import com.github.retrooper.packetevents.event.PacketReceiveEvent;
 import com.github.retrooper.packetevents.protocol.packettype.PacketType;
-import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientHeldItemChange;
+import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientKeepAlive;
 
 @CheckData(name = "DisablerE")
 public class DisablerE extends PacketCheck {
