2678a6dd34ce93e46937e9f4db0f00711547d1e4
LewUwU <72635645+LewUwU@users.noreply.github.com>
2022-03-07 07:08:56 -0400

    Update KillAuraA.java

--- src/main/java/ac/grim/grimac/checks/impl/combat/killaura/KillAuraA.java
+++ src/main/java/ac/grim/grimac/checks/impl/combat/killaura/KillAuraA.java
@@ -3,6 +3,9 @@ package ac.grim.grimac.checks.impl.combat.killaura;
 import ac.grim.grimac.checks.CheckData;
 import ac.grim.grimac.checks.type.PacketCheck;
 import ac.grim.grimac.player.GrimPlayer;
+import com.github.retrooper.packetevents.PacketEvents;
+import com.github.retrooper.packetevents.manager.server.ServerVersion;
+import com.github.retrooper.packetevents.protocol.player.ClientVersion;
 import com.github.retrooper.packetevents.event.PacketReceiveEvent;
 import com.github.retrooper.packetevents.protocol.packettype.PacketType;
 import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientInteractEntity;
@@ -12,6 +15,8 @@ import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientPl
  * You can't attack more than one unique entity within a single client tick. This check simply
  * enforces that rule by checking whether or not the player attacked an entity with different
  * IDs more than once within a single client tick.
+ *
+ * 1.9+ isn't reliable so this check is limited to 1.7 - 1.8 servers and clients
  */
 
 @CheckData(name = "KillAuraA")
@@ -24,6 +29,11 @@ public class KillAuraA extends PacketCheck {
 
     @Override
     public void onPacketReceive(PacketReceiveEvent event) {
+        boolean exempt = PacketEvents.getAPI().getServerManager().getVersion().isNewerThanOrEquals(ServerVersion.V_1_9) || 
+			                 player.getClientVersion().isNewerThanOrEquals(ClientVersion.V_1_9);
+        
+        if (exempt) return;
+        
         if (event.getPacketType() == PacketType.Play.Client.INTERACT_ENTITY) {
             WrapperPlayClientInteractEntity packet = new WrapperPlayClientInteractEntity(event);
 
