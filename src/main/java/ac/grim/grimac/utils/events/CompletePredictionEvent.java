355fba2bd973f1968d3430c9c0808e5a1b211a0c
MWHunter <s0521458@student.rockvalleycollege.edu>
2022-03-25 22:23:14 -0500

    CompletePredictionEvent now cancellable

--- src/main/java/ac/grim/grimac/utils/events/CompletePredictionEvent.java
+++ src/main/java/ac/grim/grimac/utils/events/CompletePredictionEvent.java
@@ -1,14 +1,16 @@
 package ac.grim.grimac.utils.events;
 
 import ac.grim.grimac.player.GrimPlayer;
+import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.NotNull;
 
-public class CompletePredictionEvent extends Event {
+public class CompletePredictionEvent extends Event implements Cancellable {
     private static final HandlerList handlers = new HandlerList();
     private final GrimPlayer player;
     private final double offset;
+    private boolean cancelled;
 
     public CompletePredictionEvent(GrimPlayer player, double offset) {
         super(true); // Async!
@@ -32,5 +34,15 @@ public class CompletePredictionEvent extends Event {
     @Override
     public HandlerList getHandlers() {
         return handlers;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean cancel) {
+        cancelled = cancel;
     }
 }
--- src/main/java/ac/grim/grimac/utils/events/OffsetAlertEvent.java
+++ src/main/java/ac/grim/grimac/utils/events/OffsetAlertEvent.java
@@ -1,6 +1,7 @@
 package ac.grim.grimac.utils.events;
 
 import ac.grim.grimac.checks.Check;
+import ac.grim.grimac.player.GrimPlayer;
 
 public class OffsetAlertEvent extends FlagEvent {
     private final double offset;
@@ -9,6 +10,7 @@ public class OffsetAlertEvent extends FlagEvent {
     private final double violations;
     private final boolean isAlert;
     private final boolean isSetback;
+    private final GrimPlayer player;
     private boolean cancelled;
 
     public OffsetAlertEvent(Check check, String checkName, double offset, double violations, boolean vehicle, boolean isAlert, boolean isSetback) {
@@ -19,6 +21,7 @@ public class OffsetAlertEvent extends FlagEvent {
         this.violations = violations;
         this.isAlert = isAlert;
         this.isSetback = isSetback;
+        this.player = check.getPlayer();
     }
 
     public boolean isCancelled() {
@@ -31,6 +34,10 @@ public class OffsetAlertEvent extends FlagEvent {
 
     public double getOffset() {
         return offset;
+    }
+
+    public GrimPlayer getPlayer() {
+        return player;
     }
 
     @Override
