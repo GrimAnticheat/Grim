d9cf5a074ae44b072bdcdc5759e7f27a0e82851a
MWHunter <s0521458@student.rockvalleycollege.edu>
2022-04-24 20:35:38 -0500

    mojang stupidity with 1.8 use item

--- src/main/java/ac/grim/grimac/checks/impl/movement/NoSlow.java
+++ src/main/java/ac/grim/grimac/checks/impl/movement/NoSlow.java
@@ -4,11 +4,16 @@ import ac.grim.grimac.checks.CheckData;
 import ac.grim.grimac.checks.type.PostPredictionCheck;
 import ac.grim.grimac.player.GrimPlayer;
 import ac.grim.grimac.utils.anticheat.update.PredictionComplete;
+import com.github.retrooper.packetevents.protocol.player.ClientVersion;
 
 @CheckData(name = "NoSlow (Prediction)", configName = "NoSlow", setback = 5)
 public class NoSlow extends PostPredictionCheck {
     double offsetToFlag;
     double bestOffset = 1;
+    // The player sends that they switched items the next tick if they switch from an item that can be used
+    // to another item that can be used.  What the fuck mojang.  Affects 1.8 (and most likely 1.7) clients.
+    public boolean fuckLegacyVersions = false;
+    public boolean flaggedLastTick = false;
 
     public NoSlow(GrimPlayer player) {
         super(player);
@@ -18,11 +23,21 @@ public class NoSlow extends PostPredictionCheck {
     public void onPredictionComplete(final PredictionComplete predictionComplete) {
         // If the player was using an item for certain, and their predicted velocity had a flipped item
         if (player.packetStateData.slowedByUsingItem) {
+            // 1.8 users are not slowed the first tick they use an item, strangely
+            if (player.getClientVersion().isOlderThanOrEquals(ClientVersion.V_1_8) && fuckLegacyVersions) {
+                fuckLegacyVersions = false;
+                flaggedLastTick = false;
+            }
+
             if (bestOffset > offsetToFlag) {
-                flagWithSetback();
-                alert("");
+                if (flaggedLastTick) {
+                    flagWithSetback();
+                    alert("");
+                }
+                flaggedLastTick = true;
             } else {
                 reward();
+                flaggedLastTick = false;
             }
         }
         bestOffset = 1;
