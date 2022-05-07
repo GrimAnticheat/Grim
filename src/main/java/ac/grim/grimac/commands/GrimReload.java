bc8b53a423d2df4aa85a7d8bae522fdcc0768790
AoElite <roxiehockaday@gmail.com>
2022-05-07 12:04:00 -0400

    add reload command

---
+++ src/main/java/ac/grim/grimac/commands/GrimReload.java
@@ -0,0 +1,38 @@
+package ac.grim.grimac.commands;
+
+import ac.grim.grimac.GrimAPI;
+import ac.grim.grimac.checks.Check;
+import ac.grim.grimac.player.GrimPlayer;
+import ac.grim.grimac.utils.anticheat.MessageUtil;
+import co.aikar.commands.BaseCommand;
+import co.aikar.commands.annotation.CommandAlias;
+import co.aikar.commands.annotation.CommandPermission;
+import co.aikar.commands.annotation.Subcommand;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+
+@CommandAlias("grim|grimac")
+public class GrimReload extends BaseCommand {
+    @Subcommand("reload")
+    @CommandPermission("grim.reload")
+    public void onReload(CommandSender sender) {
+
+        //reload config
+        try {
+            GrimAPI.INSTANCE.getConfigManager().reload();
+        } catch (RuntimeException e) {
+            sender.sendMessage(ChatColor.RED + e.getMessage());
+            return;
+        }
+
+        //Reload checks for all players
+        for (GrimPlayer grimPlayer : GrimAPI.INSTANCE.getPlayerDataManager().getEntries()) {
+            for (Check<?> value : grimPlayer.checkManager.allChecks.values()) {
+                value.reload();
+            }
+        }
+
+        sender.sendMessage(MessageUtil.format("%prefix% &fConfig has been reloaded."));
+    }
+
+}
