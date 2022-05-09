e34f961ee2d0ca8bd8d960442875125b18278eef
MWHunter <s0521458@student.rockvalleycollege.edu>
2022-05-09 12:05:11 -0500

    Pipeline debug

--- src/main/java/ac/grim/grimac/events/packets/PacketPlayerJoinQuit.java
+++ src/main/java/ac/grim/grimac/events/packets/PacketPlayerJoinQuit.java
@@ -2,10 +2,12 @@ package ac.grim.grimac.events.packets;
 
 import ac.grim.grimac.GrimAPI;
 import ac.grim.grimac.player.GrimPlayer;
+import ac.grim.grimac.utils.anticheat.LogUtil;
 import com.github.retrooper.packetevents.event.PacketListenerAbstract;
 import com.github.retrooper.packetevents.event.UserConnectEvent;
 import com.github.retrooper.packetevents.event.UserDisconnectEvent;
 import com.github.retrooper.packetevents.event.UserLoginEvent;
+import com.github.retrooper.packetevents.netty.channel.ChannelHelper;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
@@ -18,6 +20,9 @@ public class PacketPlayerJoinQuit extends PacketListenerAbstract {
     @Override
     public void onUserLogin(UserLoginEvent event) {
         Player player = (Player) event.getPlayer();
+        if (GrimAPI.INSTANCE.getConfigManager().getConfig().getBooleanElse("debug-pipeline-on-join", false)) {
+            LogUtil.info("Pipeline: " + ChannelHelper.pipelineHandlerNamesAsString(event.getUser().getChannel()));
+        }
         if (player.hasPermission("grim.alerts") && GrimAPI.INSTANCE.getConfigManager().getConfig().getBooleanElse("alerts.enable-on-join", true)) {
             GrimAPI.INSTANCE.getAlertManager().toggle(player);
         }
