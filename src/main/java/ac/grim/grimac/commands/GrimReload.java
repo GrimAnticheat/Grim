3823249ec3d16c0d9d2145ce805c9ed8e7e63fe6
MWHunter <s0521458@student.rockvalleycollege.edu>
2022-05-08 23:42:41 -0500

    Fix reload for punishments

--- src/main/java/ac/grim/grimac/commands/GrimReload.java
+++ src/main/java/ac/grim/grimac/commands/GrimReload.java
@@ -29,6 +29,7 @@ public class GrimReload extends BaseCommand {
         //Reload checks for all players
         for (GrimPlayer grimPlayer : GrimAPI.INSTANCE.getPlayerDataManager().getEntries()) {
             ChannelHelper.runInEventLoop(grimPlayer.user.getChannel(), () -> {
+                grimPlayer.punishmentManager.reload();
                 for (Check<?> value : grimPlayer.checkManager.allChecks.values()) {
                     value.reload();
                 }
