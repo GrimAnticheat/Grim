2c3a6acc4ebad00a91bce18a77542b1e7af39fba
MWHunter <s0521458@student.rockvalleycollege.edu>
2022-07-05 08:16:26 -0500

    Exempt 1.7 clients from BadPacketsL

--- src/main/java/ac/grim/grimac/checks/impl/badpackets/BadPacketsL.java
+++ src/main/java/ac/grim/grimac/checks/impl/badpackets/BadPacketsL.java
@@ -5,6 +5,7 @@ import ac.grim.grimac.checks.type.PacketCheck;
 import ac.grim.grimac.player.GrimPlayer;
 import com.github.retrooper.packetevents.event.PacketReceiveEvent;
 import com.github.retrooper.packetevents.protocol.packettype.PacketType;
+import com.github.retrooper.packetevents.protocol.player.ClientVersion;
 import com.github.retrooper.packetevents.protocol.player.DiggingAction;
 import com.github.retrooper.packetevents.protocol.world.BlockFace;
 import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientPlayerDigging;
@@ -21,7 +22,8 @@ public class BadPacketsL extends PacketCheck {
     public void onPacketReceive(PacketReceiveEvent event) {
         if (event.getPacketType() == PacketType.Play.Client.PLAYER_DIGGING) {
             WrapperPlayClientPlayerDigging packet = new WrapperPlayClientPlayerDigging(event);
-            if (packet.getAction() == DiggingAction.RELEASE_USE_ITEM) {
+            // 1.7 clients flag this for some reason
+            if (packet.getAction() == DiggingAction.RELEASE_USE_ITEM && player.getClientVersion().isNewerThanOrEquals(ClientVersion.V_1_8)) {
                 // The client only sends this packet in one place, with BlockPos.ZERO and Direction.DOWN
                 if (packet.getFace() != BlockFace.DOWN
                         || packet.getBlockPosition().getX() != 0
