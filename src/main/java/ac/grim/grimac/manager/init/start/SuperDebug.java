228ae2d30b63185a5493ff4a3c7c5bb295b49b9e
MWHunter <s0521458@student.rockvalleycollege.edu>
2022-07-10 16:31:57 -0500

    Fix superdebug identifier being off by one

--- src/main/java/ac/grim/grimac/manager/init/start/SuperDebug.java
+++ src/main/java/ac/grim/grimac/manager/init/start/SuperDebug.java
@@ -32,7 +32,6 @@ public final class SuperDebug extends PostPredictionCheck {
     }
 
     public static StringBuilder getFlag(int identifier) {
-        identifier--;
         if (identifier >= flags.length) return null;
         return flags[identifier];
     }
