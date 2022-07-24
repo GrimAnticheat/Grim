674b491a92de7d29f775e9b8a65d08ec795ae9fb
AoElite <20812471+AoElite@users.noreply.github.com>
2022-07-24 18:07:28 -0400

    exempt creative from AirLiquidPlace

--- src/main/java/ac/grim/grimac/checks/impl/scaffolding/AirLiquidPlace.java
+++ src/main/java/ac/grim/grimac/checks/impl/scaffolding/AirLiquidPlace.java
@@ -5,6 +5,7 @@ import ac.grim.grimac.checks.type.BlockPlaceCheck;
 import ac.grim.grimac.player.GrimPlayer;
 import ac.grim.grimac.utils.anticheat.update.BlockPlace;
 import ac.grim.grimac.utils.nmsutil.Materials;
+import com.github.retrooper.packetevents.protocol.player.GameMode;
 import com.github.retrooper.packetevents.protocol.world.states.type.StateType;
 import com.github.retrooper.packetevents.util.Vector3i;
 
@@ -16,6 +17,7 @@ public class AirLiquidPlace extends BlockPlaceCheck {
 
     @Override
     public void onBlockPlace(final BlockPlace place) {
+        if (player.gamemode == GameMode.CREATIVE) return;
         Vector3i blockPos = place.getPlacedAgainstBlockLocation();
         StateType placeAgainst = player.compensatedWorld.getStateTypeAt(blockPos.getX(), blockPos.getY(), blockPos.getZ());
 
