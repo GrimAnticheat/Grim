843840c3205188272bd8f811f6aba5e387ada74c
MWHunter <s0521458@student.rockvalleycollege.edu>
2023-01-06 19:58:04 -0600

    Improve AimModulo360 false flags

--- src/main/java/ac/grim/grimac/checks/impl/aim/AimModulo360.java
+++ src/main/java/ac/grim/grimac/checks/impl/aim/AimModulo360.java
@@ -19,12 +19,12 @@ public class AimModulo360 extends Check implements RotationCheck {
 
     @Override
     public void process(final RotationUpdate rotationUpdate) {
+        lastDeltaYaw = rotationUpdate.getDeltaXRot();
         if (player.packetStateData.lastPacketWasTeleport) return;
         if (player.xRot < 360 && player.xRot > -360 && Math.abs(rotationUpdate.getDeltaXRot()) > 320 && Math.abs(lastDeltaYaw) < 30) {
             flagAndAlert();
         } else {
             reward();
         }
-        lastDeltaYaw = rotationUpdate.getDeltaXRot();
     }
 }
