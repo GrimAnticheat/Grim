d99ba2840beaa0526dc9aa549f0462448868bb5d
Lew <skycubesv@gmail.com>
2023-02-17 04:13:45 -0600

    Cleanup imports

--- src/main/java/ac/grim/grimac/checks/impl/badpackets/BadPacketsQ.java
+++ src/main/java/ac/grim/grimac/checks/impl/badpackets/BadPacketsQ.java
@@ -6,10 +6,7 @@ import ac.grim.grimac.checks.type.PacketCheck;
 import ac.grim.grimac.player.GrimPlayer;
 import com.github.retrooper.packetevents.event.PacketReceiveEvent;
 import com.github.retrooper.packetevents.protocol.packettype.PacketType;
-import com.github.retrooper.packetevents.protocol.player.GameMode;
-import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientHeldItemChange;
 import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientInteractEntity;
-import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientPlayerAbilities;
 import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientPlayerFlying;
 
 @CheckData(name = "BadPacketsQ")
