899c805b93b9f5e6852da4fb741ba08bf402abbe
noobi <yuuji1305@gmail.com>
2023-02-23 18:11:29 +0100

    Create BadPacketsM.java

---
+++ src/main/java/ac/grim/grimac/checks/impl/badpackets/BadPacketsM.java
@@ -0,0 +1,31 @@
+package ac.grim.grimac.checks.impl.badpackets;
+
+import ac.grim.grimac.checks.Check;
+import ac.grim.grimac.checks.CheckData;
+import ac.grim.grimac.checks.type.PacketCheck;
+import ac.grim.grimac.player.GrimPlayer;
+import com.github.retrooper.packetevents.event.PacketReceiveEvent;
+import com.github.retrooper.packetevents.protocol.packettype.PacketType;
+import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientPlayerFlying;
+
+@CheckData(name = "BadPacketsM", experimental = true)
+public class BadPacketsM extends Check implements PacketCheck {
+    public BadPacketsM(GrimPlayer player) {
+        super(player);
+    }
+
+    private int i = 0;
+
+    @Override
+    public void onPacketReceive(PacketReceiveEvent event) {
+        if (WrapperPlayClientPlayerFlying.isFlying(event.getPacketType())) {
+            i = 0;
+        } else if (event.getPacketType() == PacketType.Play.Client.PLAYER_BLOCK_PLACEMENT) {
+            i++;
+        } else if (event.getPacketType() == PacketType.Play.Client.PLAYER_DIGGING) {
+            if (i > 0) {
+                flagAndAlert();
+            }
+        }
+    }
+}
