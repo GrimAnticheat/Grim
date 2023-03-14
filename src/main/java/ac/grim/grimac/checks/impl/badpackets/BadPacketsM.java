13ac60a1d6377da936bc0637d952680fc87ea96b
noobi <yuuji1305@gmail.com>
2023-03-14 05:24:39 +0100

    Use onPredictionComplete

--- src/main/java/ac/grim/grimac/checks/impl/badpackets/BadPacketsM.java
+++ src/main/java/ac/grim/grimac/checks/impl/badpackets/BadPacketsM.java
@@ -2,8 +2,9 @@ package ac.grim.grimac.checks.impl.badpackets;
 
 import ac.grim.grimac.checks.Check;
 import ac.grim.grimac.checks.CheckData;
-import ac.grim.grimac.checks.type.PacketCheck;
+import ac.grim.grimac.checks.type.PostPredictionCheck;
 import ac.grim.grimac.player.GrimPlayer;
+import ac.grim.grimac.utils.anticheat.update.PredictionComplete;
 import com.github.retrooper.packetevents.event.PacketReceiveEvent;
 import com.github.retrooper.packetevents.protocol.packettype.PacketType.Play.Client;
 import com.github.retrooper.packetevents.protocol.packettype.PacketTypeCommon;
@@ -13,12 +14,20 @@ import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientPl
 import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientPlayerFlying;
 
 @CheckData(name = "BadPacketsM", experimental = true)
-public class BadPacketsM extends Check implements PacketCheck {
+public class BadPacketsM extends Check implements PostPredictionCheck {
     public BadPacketsM(GrimPlayer player) {
         super(player);
     }
 
     private boolean hasPlacedBlock;
+
+    @Override
+    public void onPredictionComplete(PredictionComplete predictionComplete) {
+        // call onPredictionComplete to get updated value of isTickingReliablyFor
+        if (player.getClientVersion().isNewerThan(ClientVersion.V_1_8) && (!player.isTickingReliablyFor(3) || !player.skippedTickInActualMovement)) {
+            hasPlacedBlock = false;
+        }
+    }
 
     @Override
     public void onPacketReceive(PacketReceiveEvent event) {
@@ -31,19 +40,12 @@ public class BadPacketsM extends Check implements PacketCheck {
         }
 
         // early returning if packet types do not match
-        if (!packetType.equals(Client.PLAYER_DIGGING) || !packetType.equals(Client.PLAYER_BLOCK_PLACEMENT)) {
-            return;
-        }
-
-        // isTickingReliablyFor had falses so we added more conditions to actually check if the player was moving for 1.8+
-        if (player.getClientVersion().isNewerThan(ClientVersion.V_1_8) && (!player.isTickingReliablyFor(3)
-                || player.actualMovement.lengthSquared() <= 0.000064 || !player.skippedTickInActualMovement)) {
-            hasPlacedBlock = false;
+        if (packetType != Client.PLAYER_BLOCK_PLACEMENT && packetType != Client.PLAYER_DIGGING)  {
             return;
         }
 
         // since order is important we only set true when PLAYER_BLOCK_PLACEMENT
-        if (packetType.equals(Client.PLAYER_BLOCK_PLACEMENT)) {
+        if (packetType == Client.PLAYER_BLOCK_PLACEMENT) {
             hasPlacedBlock = true;
         } else if (hasPlacedBlock) {
             DiggingAction action = new WrapperPlayClientPlayerDigging(event).getAction();
