07ac8b99f083decf46fbde0eec2301887111823e
noobi <yuuji1305@gmail.com>
2023-03-19 00:16:05 +0100

    Implement InventoryC (Placing while inventory is open)

---
+++ src/main/java/ac/grim/grimac/checks/impl/inventory/InventoryC.java
@@ -0,0 +1,35 @@
+package ac.grim.grimac.checks.impl.inventory;
+
+import ac.grim.grimac.checks.Check;
+import ac.grim.grimac.checks.CheckData;
+import ac.grim.grimac.checks.type.PacketCheck;
+import ac.grim.grimac.player.GrimPlayer;
+import com.github.retrooper.packetevents.event.PacketReceiveEvent;
+import com.github.retrooper.packetevents.protocol.packettype.PacketType;
+
+@CheckData(name = "InventoryC")
+public class InventoryC extends Check implements PacketCheck {
+
+    public InventoryC(GrimPlayer player) {
+        super(player);
+    }
+
+    @Override
+    public void onPacketReceive(PacketReceiveEvent event) {
+        if (event.getPacketType() == PacketType.Play.Client.PLAYER_BLOCK_PLACEMENT) {
+            // It is not possible to place a block while the inventory is open
+            if (player.hasInventoryOpen) {
+                if (flagWithSetback()) {
+                    // Cancel the packet
+                    if (shouldModifyPackets()) {
+                        event.setCancelled(true);
+                        player.onPacketCancel();
+                    }
+                    alert("");
+                }
+            } else {
+                reward();
+            }
+        }
+    }
+}
