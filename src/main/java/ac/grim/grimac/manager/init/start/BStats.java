58fade63557a2781dd477697c613a00fadcf2eba
RealHirn <github@realhirn.de>
2023-03-27 18:06:26 +0200

    Update Bstats

--- src/main/java/ac/grim/grimac/manager/init/start/BStats.java
+++ src/main/java/ac/grim/grimac/manager/init/start/BStats.java
@@ -7,7 +7,7 @@ import org.bstats.bukkit.Metrics;
 public class BStats implements Initable {
     @Override
     public void start() {
-        int pluginId = 12820; // <-- Replace with the id of your plugin!
+        int pluginId = 18077; // <-- Replace with the id of your plugin!
         try {
             Metrics metrics = new Metrics(GrimAPI.INSTANCE.getPlugin(), pluginId);
         } catch (Exception ignored) {
