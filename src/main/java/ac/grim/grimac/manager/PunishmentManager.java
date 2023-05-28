bebcdf102a4d66501a11da8e6c157a94684002e9
Amaury Rousseau <kinrar@kinrar.io>
2023-05-28 02:43:16 +0200

    Always execute commands when reaching the threshold

--- src/main/java/ac/grim/grimac/manager/PunishmentManager.java
+++ src/main/java/ac/grim/grimac/manager/PunishmentManager.java
@@ -131,7 +131,7 @@ public class PunishmentManager {
                     if (violationCount >= command.getThreshold()) {
                         // 0 means execute once
                         // Any other number means execute every X interval
-                        boolean inInterval = command.getInterval() == 0 ? (command.executeCount == 0) : (violationCount % command.getInterval() == 0);
+                        boolean inInterval = command.getInterval() == 0 ? (command.executeCount == 0) : ((violationCount - command.getThreshold()) % command.getInterval() == 0);
                         if (inInterval) {
                             CommandExecuteEvent executeEvent = new CommandExecuteEvent(player, check, cmd);
                             Bukkit.getPluginManager().callEvent(executeEvent);
