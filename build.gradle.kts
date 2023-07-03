cb7788e39a39cff42807a4aff5dbae7c7f8b6cc7
ViaBackwards <96089181+ViaBackwards@users.noreply.github.com>
2023-07-03 01:04:53 +0100

    oops

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,7 +42,7 @@ dependencies {
     implementation("github.scarsz:configuralize:1.4.0")
     implementation("com.github.puregero:multilib:1.1.8")
 
-    implementation(files("C:\\Users\\User\\Documents\\GitHub\\GrimAPI\\build\\libs\\GrimAPI-1.0.jar"))
+    implementation("com.github.grimanticheat:grimapi:ad8911a9c0")
     // Used for local testing: implementation("ac.grim.grimac:grimapi:1.0")
 
     compileOnly("org.geysermc.floodgate:api:2.0-SNAPSHOT")
