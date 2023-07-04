ae397301c7ef98fde283a5bf7929eb6a9f39b0d4
ViaBackwards <96089181+ViaBackwards@users.noreply.github.com>
2023-07-04 13:31:32 +0100

    hasn't been tested pre-1.17

--- src/main/java/ac/grim/grimac/checks/impl/combat/UseHit.java
+++ src/main/java/ac/grim/grimac/checks/impl/combat/UseHit.java
@@ -6,6 +6,7 @@ import ac.grim.grimac.checks.type.PacketCheck;
 import ac.grim.grimac.player.GrimPlayer;
 import com.github.retrooper.packetevents.event.PacketReceiveEvent;
 import com.github.retrooper.packetevents.protocol.packettype.PacketType;
+import com.github.retrooper.packetevents.protocol.player.ClientVersion;
 import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientInteractEntity;
 
 @CheckData(name = "UseHit")
@@ -19,6 +20,9 @@ public class UseHit extends Check implements PacketCheck {
 
     @Override
     public void onPacketReceive(PacketReceiveEvent event) {
+        // This check hasn't been tested on versions older before 1.17
+        if (player.getClientVersion().isOlderThan(ClientVersion.V_1_17)) return;
+        
         if (!player.disableGrim && event.getPacketType() == PacketType.Play.Client.INTERACT_ENTITY) {
             if (isUsingItem && movementPacketsSinceChange > 1) {
                 WrapperPlayClientInteractEntity action = new WrapperPlayClientInteractEntity(event);
