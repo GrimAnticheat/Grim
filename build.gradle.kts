d60c5577ebc36108f052f46dd7882eeec2e3fdfe
AoElite <20812471+AoElite@users.noreply.github.com>
2024-02-27 15:48:55 -0500

    update packetevent settings & bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 
 
 group = "ac.grim.grimac"
-version = "2.3.58"
+version = "2.3.59"
 description = "Libre simulation anticheat designed for 1.20 with 1.8-1.20 support, powered by PacketEvents 2.0."
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 java.targetCompatibility = JavaVersion.VERSION_1_8
