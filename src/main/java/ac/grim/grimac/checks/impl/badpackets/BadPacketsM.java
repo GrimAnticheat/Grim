db6f887325d1839f8c58dfb0c9a5a4c0b8f0bb43
ManInMyVan <maninmyvan@gmail.com>
2024-04-11 18:26:47 -0600

    Add BadPacketsM

---
+++ src/main/java/ac/grim/grimac/checks/impl/badpackets/BadPacketsM.java
@@ -0,0 +1,45 @@
+package ac.grim.grimac.checks.impl.badpackets;
+
+import ac.grim.grimac.checks.Check;
+import ac.grim.grimac.checks.CheckData;
+import ac.grim.grimac.checks.type.PacketCheck;
+import ac.grim.grimac.player.GrimPlayer;
+import com.github.retrooper.packetevents.event.PacketReceiveEvent;
+import com.github.retrooper.packetevents.protocol.packettype.PacketType;
+import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientInteractEntity;
+
+@CheckData(name = "BadPacketsM")
+public class BadPacketsM extends Check implements PacketCheck {
+    public BadPacketsM(final GrimPlayer player) {
+        super(player);
+    }
+
+    private boolean sentInteractAt = false;
+
+    @Override
+    public void onPacketReceive(PacketReceiveEvent event) {
+        if (event.getPacketType() == PacketType.Play.Client.INTERACT_ENTITY) {
+            switch (new WrapperPlayClientInteractEntity(event).getAction()) {
+                // INTERACT_AT then INTERACT
+                case INTERACT:
+                    if (!sentInteractAt) {
+                        if (flagAndAlert("Missed Interact-At")) {
+                            event.setCancelled(true);
+                            player.onPacketCancel();
+                        }
+                    }
+                    sentInteractAt = false;
+                    break;
+                case INTERACT_AT:
+                    if (sentInteractAt) {
+                        if (flagAndAlert("Missed Interact")) {
+                            event.setCancelled(true);
+                            player.onPacketCancel();
+                        }
+                    }
+                    sentInteractAt = true;
+                    break;
+            }
+        }
+    }
+}
