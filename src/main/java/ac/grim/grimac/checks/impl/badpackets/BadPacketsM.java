62f30dff579b3443bc87d774207325e9f7f4f0f3
ManInMyVan <maninmyvan@gmail.com>
2024-04-12 12:40:31 -0600

    exempt 1.7 players in BadPacketsM

--- src/main/java/ac/grim/grimac/checks/impl/badpackets/BadPacketsM.java
+++ src/main/java/ac/grim/grimac/checks/impl/badpackets/BadPacketsM.java
@@ -6,6 +6,7 @@ import ac.grim.grimac.checks.type.PacketCheck;
 import ac.grim.grimac.player.GrimPlayer;
 import com.github.retrooper.packetevents.event.PacketReceiveEvent;
 import com.github.retrooper.packetevents.protocol.packettype.PacketType;
+import com.github.retrooper.packetevents.protocol.player.ClientVersion;
 import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientInteractEntity;
 
 @CheckData(name = "BadPacketsM", experimental = true)
@@ -14,11 +15,14 @@ public class BadPacketsM extends Check implements PacketCheck {
         super(player);
     }
 
+    // 1.7 players do not send INTERACT_AT, so we cannot check them
+    private final boolean exempt = player.getClientVersion().isOlderThanOrEquals(ClientVersion.V_1_7_10);
     private boolean sentInteractAt = false;
 
     @Override
     public void onPacketReceive(PacketReceiveEvent event) {
         if (event.getPacketType() == PacketType.Play.Client.INTERACT_ENTITY) {
+            if (exempt) return;
             switch (new WrapperPlayClientInteractEntity(event).getAction()) {
                 // INTERACT_AT then INTERACT
                 case INTERACT:
