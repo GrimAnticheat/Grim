a83783c127266a3e94f84bb480173d97d11a9434
AoElite <20812471+AoElite@users.noreply.github.com>
2024-04-15 20:18:26 -0400

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,14 +3,14 @@
 plugins {
     id("java")
     id("maven-publish")
-    id("com.github.johnrengelman.shadow") version "7.1.2"
-    id("io.freefair.lombok") version "6.6"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("io.freefair.lombok") version "8.6"
     //id("net.minecrell.plugin-yml.bukkit") version "0.5.2"
 }
 
 
 group = "ac.grim.grimac"
-version = "2.3.61"
+version = "2.3.62"
 description = "Libre simulation anticheat designed for 1.20 with 1.8-1.20 support, powered by PacketEvents 2.0."
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 java.targetCompatibility = JavaVersion.VERSION_1_8
@@ -23,6 +23,7 @@ repositories {
     maven("https://repo.aikar.co/content/groups/aikar/") // ACF
     maven("https://nexus.scarsz.me/content/repositories/releases") // Configuralize
     maven("https://repo.opencollab.dev/maven-snapshots/") // Floodgate
+    maven("https://repo.opencollab.dev/maven-releases/") // Cumulus (for Floodgate)
     maven("https://repo.codemc.io/repository/maven-releases/") // PacketEvents
     mavenCentral()
     // FastUtil, Discord-Webhooks
@@ -33,15 +34,16 @@ dependencies {
     implementation("co.aikar:acf-paper:0.5.1-SNAPSHOT")
     implementation("club.minnced:discord-webhooks:0.8.0") // Newer versions include kotlin-stdlib, which leads to incompatibility with plugins that use Kotlin
     implementation("it.unimi.dsi:fastutil:8.5.13")
-    implementation("org.jetbrains:annotations:23.1.0") // Why is this needed to compile?
     implementation("github.scarsz:configuralize:1.4.0")
 
     implementation("com.github.grimanticheat:grimapi:9f5aaef74b")
     // Used for local testing: implementation("ac.grim.grimac:grimapi:1.0")
 
+    implementation("org.jetbrains:annotations:24.1.0")
     compileOnly("org.geysermc.floodgate:api:2.0-SNAPSHOT")
     compileOnly("org.spigotmc:spigot-api:1.19.3-R0.1-SNAPSHOT")
-    compileOnly("com.viaversion:viaversion-api:4.1.1")
+    compileOnly("com.viaversion:viaversion-api:4.9.4-SNAPSHOT")
+    //
     compileOnly("io.netty:netty-all:4.1.85.Final")
 }
 
