1676c35c2c9d99dc276b1dcfa5a2054d3e306d20
SamB440 <sam@islandearth.net>
2024-05-26 20:30:58 +0100

    Fix inaccuracy in entity position tracking

--- src/main/java/ac/grim/grimac/utils/anticheat/update/BlockPlace.java
+++ src/main/java/ac/grim/grimac/utils/anticheat/update/BlockPlace.java
@@ -583,7 +583,7 @@ public class BlockPlace {
                     // This happens due to the lack of an idle packet on 1.9+ clients
                     // On 1.8 clients this should practically never happen
                     if (interpWidth - width > 0.05 || interpHeight - height > 0.05) {
-                        Vector3d entityPos = entity.desyncClientPos;
+                        Vector3d entityPos = entity.trackedServerPosition.getPos();
                         interpBox = GetBoundingBox.getPacketEntityBoundingBox(player, entityPos.getX(), entityPos.getY(), entityPos.getZ(), entity);
                     }
 
