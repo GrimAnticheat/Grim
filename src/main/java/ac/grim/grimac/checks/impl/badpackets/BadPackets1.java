ea88d4ff4ff6747d03def2b4bc79662791874e86
ManInMyVan <maninmyvan@gmail.com>
2024-05-27 17:37:52 -0600

    update
    - use packet pos
    - exempt 1.17 duplicates
    - cancel the packet
    - exempt on teleport less

--- src/main/java/ac/grim/grimac/checks/impl/badpackets/BadPackets1.java
+++ src/main/java/ac/grim/grimac/checks/impl/badpackets/BadPackets1.java
@@ -6,6 +6,8 @@ import ac.grim.grimac.checks.type.PacketCheck;
 import ac.grim.grimac.player.GrimPlayer;
 import com.github.retrooper.packetevents.event.PacketReceiveEvent;
 import com.github.retrooper.packetevents.protocol.packettype.PacketType;
+import com.github.retrooper.packetevents.protocol.player.ClientVersion;
+import com.github.retrooper.packetevents.util.Vector3d;
 import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientPlayerFlying;
 
 @CheckData(name = "BadPackets1", experimental = true)
@@ -14,20 +16,26 @@ public class BadPackets1 extends Check implements PacketCheck {
         super(player);
     }
 
-    private boolean lastLastPacketWasTeleport = true;
+    private boolean lastLastPacketWasTeleport = player.getClientVersion().isOlderThanOrEquals(ClientVersion.V_1_16_4);
 
     @Override
     public void onPacketReceive(PacketReceiveEvent event) {
         if (event.getPacketType() == PacketType.Play.Client.PLAYER_POSITION || event.getPacketType() == PacketType.Play.Client.PLAYER_POSITION_AND_ROTATION) {
-            final double delta = Math.sqrt((player.x - player.lastX) * (player.x - player.lastX) + (player.y - player.lastY) * (player.y - player.lastY) + (player.z - player.lastZ) * (player.z - player.lastZ));
-            // This seems to be valid for 1.17 duplicates, don't exempt
-            if (!player.packetStateData.lastPacketWasTeleport && !lastLastPacketWasTeleport && player.checkManager.getPacketCheck(BadPacketsE.class).noReminderTicks < 20 && !player.skippedTickInActualMovement) {
-                if (delta <= player.getMovementThreshold() && flagWithSetback()) alert("delta=" + delta);
+            if (!player.packetStateData.lastPacketWasOnePointSeventeenDuplicate && !player.packetStateData.lastPacketWasTeleport && !lastLastPacketWasTeleport && player.checkManager.getPacketCheck(BadPacketsE.class).noReminderTicks < 20 && !player.skippedTickInActualMovement) {
+                final double delta = new WrapperPlayClientPlayerFlying(event).getLocation().getPosition().distance(new Vector3d(player.lastX, player.lastY, player.lastZ));
+                if (delta <= player.getMovementThreshold() && flagWithSetback()) {
+                    alert("delta=" + delta);
+                    if (shouldModifyPackets()) {
+                        event.setCancelled(true);
+                        player.onPacketCancel();
+                    }
+                }
             }
         }
 
         if (WrapperPlayClientPlayerFlying.isFlying(event.getPacketType())) {
-            lastLastPacketWasTeleport = player.packetStateData.lastPacketWasTeleport;
+            // for lunar & some forge clients. Stopped between 1.13 and 1.16.5, exempt pre-1.16.5 just to be safe
+            lastLastPacketWasTeleport = player.packetStateData.lastPacketWasTeleport && player.getClientVersion().isOlderThanOrEquals(ClientVersion.V_1_16_4);
         }
     }
 }
