43318d2a89637c2b185aa32cc7b98512503af4e9
Dg32z_ <a1410521748@hotmail.com>
2024-06-30 21:19:57 +0800

    Check for unknown client versions for players

--- src/main/java/ac/grim/grimac/GrimAPI.java
+++ src/main/java/ac/grim/grimac/GrimAPI.java
@@ -2,11 +2,18 @@ package ac.grim.grimac;
 
 import ac.grim.grimac.api.GrimAbstractAPI;
 import ac.grim.grimac.manager.*;
+import ac.grim.grimac.player.GrimPlayer;
 import ac.grim.grimac.utils.anticheat.PlayerDataManager;
+import com.github.retrooper.packetevents.protocol.ConnectionState;
+import com.github.retrooper.packetevents.protocol.player.ClientVersion;
+import com.github.retrooper.packetevents.protocol.player.User;
+import com.github.retrooper.packetevents.protocol.player.UserProfile;
 import lombok.Getter;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.ServicePriority;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import static java.util.UUID.randomUUID;
 
 @Getter
 public enum GrimAPI {
@@ -27,6 +34,11 @@ public enum GrimAPI {
         this.configManager = new ConfigManager();
         initManager = new InitManager();
         initManager.load();
+        User userCannotJoin = new User(
+                null, ConnectionState.LOGIN, ClientVersion.UNKNOWN, //Invalid client version
+                new UserProfile(randomUUID(), "USER_CANNOT_JOIN")
+        );
+        new GrimPlayer(userCannotJoin);
     }
 
     public void start(final JavaPlugin plugin) {
