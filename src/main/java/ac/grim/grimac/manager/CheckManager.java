f097580e90dd11df84b62b3dcb5f5b5085eab947
ManInMyVan <maninmyvan@gmail.com>
2024-07-14 11:43:55 -0600

    Add BadPackets2

--- src/main/java/ac/grim/grimac/manager/CheckManager.java
+++ src/main/java/ac/grim/grimac/manager/CheckManager.java
@@ -124,6 +124,7 @@ public class CheckManager {
                 .put(SuperDebug.class, new SuperDebug(player))
                 .put(DebugHandler.class, new DebugHandler(player))
                 .put(EntityControl.class, new EntityControl(player))
+                .put(BadPackets2.class, new BadPackets2(player))
                 .put(NoSlowA.class, new NoSlowA(player))
                 .put(SetbackTeleportUtil.class, new SetbackTeleportUtil(player)) // Avoid teleporting to new position, update safe pos last
                 .put(CompensatedFireworks.class, player.compensatedFireworks)
