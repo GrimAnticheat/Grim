5818f5b2832fbfb52ab0fcf2d0719861a45d2617
PaguroCavaliere <136935678+PaguroCavaliere@users.noreply.github.com>
2024-08-20 18:57:32 +0200

    Fix false
    ^ the goal of this check is to check if the user is looking at the correct face of the block to place a subsequent block. Consequently this "fix false" is more of a clarification to make this check do what it is supposed to do. It is purely linked to scaffolding and not to briding in general. This fix eliminates false and does not reduce the effectiveness of the check

--- src/main/java/ac/grim/grimac/checks/impl/scaffolding/CorrectPlace.java
+++ src/main/java/ac/grim/grimac/checks/impl/scaffolding/CorrectPlace.java
@@ -5,6 +5,9 @@ import ac.grim.grimac.checks.type.BlockPlaceCheck;
 import ac.grim.grimac.player.GrimPlayer;
 import ac.grim.grimac.utils.anticheat.update.BlockPlace;
 import com.github.retrooper.packetevents.util.Vector3i;
+import io.github.retrooper.packetevents.util.SpigotConversionUtil;
+import org.bukkit.Location;
+import org.bukkit.Material;
 
 @CheckData(name = "CorrectPlace", experimental = true)
 public class CorrectPlace extends BlockPlaceCheck {
@@ -19,6 +22,12 @@ public class CorrectPlace extends BlockPlaceCheck {
     public void onBlockPlace(BlockPlace event) {
         if (player.y < event.getPlacedBlockPos().getY()) return;
         if (player.yRot < 82.6 && player.yRot > 76.7) return;
+
+        Vector3i pos = event.getPlacedBlockPos();
+
+        Location loc = new Location(player.bukkitPlayer.getWorld(), pos.getX(), pos.getY(), pos.getZ());
+
+        if (!loc.subtract(0, 0.6, 0).getBlock().getType().equals(Material.AIR)) return;
 
         if (previousBlockPlaced != null && previousBlockPlaced.getY() - event.getPlacedBlockPos().getY() == 0) {
             if (flagAndAlert() && shouldCancel() && shouldModifyPackets()) {
