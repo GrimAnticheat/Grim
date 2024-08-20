1ec7801403725a6034dd3bb203a7094dc2b9afea
PaguroCavaliere <136935678+PaguroCavaliere@users.noreply.github.com>
2024-08-20 19:54:25 +0200

    Used compensatedWorld instead of Bukkit

--- src/main/java/ac/grim/grimac/checks/impl/scaffolding/CorrectPlace.java
+++ src/main/java/ac/grim/grimac/checks/impl/scaffolding/CorrectPlace.java
@@ -4,10 +4,8 @@ import ac.grim.grimac.checks.CheckData;
 import ac.grim.grimac.checks.type.BlockPlaceCheck;
 import ac.grim.grimac.player.GrimPlayer;
 import ac.grim.grimac.utils.anticheat.update.BlockPlace;
+import com.github.retrooper.packetevents.util.Vector3d;
 import com.github.retrooper.packetevents.util.Vector3i;
-import io.github.retrooper.packetevents.util.SpigotConversionUtil;
-import org.bukkit.Location;
-import org.bukkit.Material;
 
 @CheckData(name = "CorrectPlace", experimental = true)
 public class CorrectPlace extends BlockPlaceCheck {
@@ -23,11 +21,9 @@ public class CorrectPlace extends BlockPlaceCheck {
         if (player.y < event.getPlacedBlockPos().getY()) return;
         if (player.yRot < 82.6 && player.yRot > 76.7) return;
 
-        Vector3i pos = event.getPlacedBlockPos();
+        Vector3d pos = event.getPlacedBlockPos().toVector3d().subtract(0, 0.6, 0);
 
-        Location loc = new Location(player.bukkitPlayer.getWorld(), pos.getX(), pos.getY(), pos.getZ());
-
-        if (!loc.subtract(0, 0.6, 0).getBlock().getType().equals(Material.AIR)) return;
+        if (!player.compensatedWorld.getWrappedBlockStateAt(pos.toVector3i()).getType().isAir()) return;
 
         if (previousBlockPlaced != null && previousBlockPlaced.getY() - event.getPlacedBlockPos().getY() == 0) {
             if (flagAndAlert() && shouldCancel() && shouldModifyPackets()) {
