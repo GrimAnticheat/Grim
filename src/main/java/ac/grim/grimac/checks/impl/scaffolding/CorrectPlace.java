bf0a81fca3591d5b9edad42175c52aceb31a9ab4
PaguroCavaliere <136935678+PaguroCavaliere@users.noreply.github.com>
2024-08-20 23:57:15 +0200

    This should fix telly bridge issue and other false

--- src/main/java/ac/grim/grimac/checks/impl/scaffolding/CorrectPlace.java
+++ src/main/java/ac/grim/grimac/checks/impl/scaffolding/CorrectPlace.java
@@ -5,12 +5,15 @@ import ac.grim.grimac.checks.type.BlockPlaceCheck;
 import ac.grim.grimac.player.GrimPlayer;
 import ac.grim.grimac.utils.anticheat.update.BlockPlace;
 import com.github.retrooper.packetevents.util.Vector3d;
+import com.github.retrooper.packetevents.util.Vector3f;
 import com.github.retrooper.packetevents.util.Vector3i;
 
 @CheckData(name = "CorrectPlace", experimental = true)
 public class CorrectPlace extends BlockPlaceCheck {
 
     private Vector3i previousBlockPlaced = null;
+
+    private double maxPitch = 0; // when player is Jumping can place a block with a 85.4
 
     public CorrectPlace(GrimPlayer player) {
         super(player);
@@ -19,10 +22,19 @@ public class CorrectPlace extends BlockPlaceCheck {
     @Override
     public void onBlockPlace(BlockPlace event) {
         if (player.y < event.getPlacedBlockPos().getY()) return;
-        if (player.yRot < 82.6 && player.yRot > 76.7) return;
+
+        Vector3f cursor = event.getCursor();
+
+        // player is looking at the end of block (at left or right), this won't affect the center
+        if ((cursor.getX() < 0.2 && cursor.getX() > 0.8) || (cursor.getZ() < 0.2 && cursor.getZ() > 0.8)) return;
+
+        double diffY = player.y - event.getPlacedBlockPos().toVector3d().getY();
+
+        maxPitch = 2.33d * diffY + 80.97d;
+
+        if (player.yRot < maxPitch && player.yRot > 74.5) return;
 
         Vector3d pos = event.getPlacedBlockPos().toVector3d().subtract(0, 0.6, 0);
-
         if (!player.compensatedWorld.getWrappedBlockStateAt(pos.toVector3i()).getType().isAir()) return;
 
         if (previousBlockPlaced != null && previousBlockPlaced.getY() - event.getPlacedBlockPos().getY() == 0) {
@@ -32,5 +44,4 @@ public class CorrectPlace extends BlockPlaceCheck {
         }
         previousBlockPlaced = event.getPlacedBlockPos();
     }
-
 }
