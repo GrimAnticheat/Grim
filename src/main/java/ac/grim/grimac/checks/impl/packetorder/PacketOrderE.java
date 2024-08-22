419e89317f692a33bb48a668f65e1d0aba3fc2ff
ManInMyVan <maninmyvan@gmail.com>
2024-08-22 11:58:23 -0600

    add PacketOrderE

---
+++ src/main/java/ac/grim/grimac/checks/impl/packetorder/PacketOrderE.java
@@ -0,0 +1,71 @@
+package ac.grim.grimac.checks.impl.packetorder;
+
+import ac.grim.grimac.checks.Check;
+import ac.grim.grimac.checks.CheckData;
+import ac.grim.grimac.checks.type.PacketCheck;
+import ac.grim.grimac.checks.type.PostPredictionCheck;
+import ac.grim.grimac.player.GrimPlayer;
+import ac.grim.grimac.utils.anticheat.update.PredictionComplete;
+import com.github.retrooper.packetevents.event.PacketReceiveEvent;
+import com.github.retrooper.packetevents.protocol.packettype.PacketType;
+import com.github.retrooper.packetevents.protocol.player.ClientVersion;
+import com.github.retrooper.packetevents.protocol.player.DiggingAction;
+import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientPlayerDigging;
+import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientPlayerFlying;
+
+@CheckData(name = "PacketOrderE", experimental = true)
+public class PacketOrderE extends Check implements PostPredictionCheck {
+    public PacketOrderE(final GrimPlayer player) {
+        super(player);
+    }
+
+    private int invalidSlots = 0;
+    private boolean sent = false;
+
+    @Override
+    public void onPacketReceive(PacketReceiveEvent event) {
+        if (event.getPacketType() == PacketType.Play.Client.HELD_ITEM_CHANGE) {
+            if (sent) {
+                if (player.getClientVersion().isOlderThanOrEquals(ClientVersion.V_1_8)) {
+                    flagAndAlert();
+                } else {
+                    invalidSlots++;
+                }
+            }
+        }
+
+        if (event.getPacketType() == PacketType.Play.Client.PLAYER_BLOCK_PLACEMENT
+                || event.getPacketType() == PacketType.Play.Client.USE_ITEM
+                || event.getPacketType() == PacketType.Play.Client.INTERACT_ENTITY
+                || event.getPacketType() == PacketType.Play.Client.ENTITY_ACTION
+        ) {
+            sent = true;
+        }
+
+        if (event.getPacketType() == PacketType.Play.Client.PLAYER_DIGGING) {
+            DiggingAction action = new WrapperPlayClientPlayerDigging(event).getAction();
+            if (action != DiggingAction.CANCELLED_DIGGING && action != DiggingAction.START_DIGGING && action != DiggingAction.FINISHED_DIGGING) {
+                sent = true;
+            }
+        }
+
+        if (WrapperPlayClientPlayerFlying.isFlying(event.getPacketType()) && player.getClientVersion().isOlderThanOrEquals(ClientVersion.V_1_8) && !player.packetStateData.lastPacketWasTeleport) {
+            sent = false;
+        }
+    }
+
+    @Override
+    public void onPredictionComplete(PredictionComplete predictionComplete) {
+        // we don't need to check pre-1.9 players here (no tick skipping)
+        if (player.getClientVersion().isOlderThanOrEquals(ClientVersion.V_1_8)) return;
+
+        if (!player.skippedTickInActualMovement) {
+            for (; invalidSlots >= 1; invalidSlots--) {
+                flagAndAlert();
+            }
+        }
+
+        invalidSlots = 0;
+        sent = false;
+    }
+}
