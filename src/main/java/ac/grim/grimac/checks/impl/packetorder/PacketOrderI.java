4f1432dc3dc330b5bfba2486b38f46605dc8fb2c
ManInMyVan <maninmyvan@gmail.com>
2024-08-28 12:14:54 -0600

    Add PacketOrderI

---
+++ src/main/java/ac/grim/grimac/checks/impl/packetorder/PacketOrderI.java
@@ -0,0 +1,64 @@
+package ac.grim.grimac.checks.impl.packetorder;
+
+import ac.grim.grimac.checks.Check;
+import ac.grim.grimac.checks.CheckData;
+import ac.grim.grimac.checks.type.PostPredictionCheck;
+import ac.grim.grimac.player.GrimPlayer;
+import ac.grim.grimac.utils.anticheat.update.PredictionComplete;
+import com.github.retrooper.packetevents.event.PacketReceiveEvent;
+import com.github.retrooper.packetevents.protocol.packettype.PacketType;
+import com.github.retrooper.packetevents.protocol.player.ClientVersion;
+import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientInteractEntity;
+import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientPlayerFlying;
+
+@CheckData(name = "PacketOrderI", experimental = true)
+public class PacketOrderI extends Check implements PostPredictionCheck {
+    public PacketOrderI(final GrimPlayer player) {
+        super(player);
+    }
+
+    private int invalid = 0;
+    private boolean sent = false;
+
+    @Override
+    public void onPacketReceive(PacketReceiveEvent event) {
+        if (event.getPacketType() == PacketType.Play.Client.INTERACT_ENTITY) {
+            switch (new WrapperPlayClientInteractEntity(event).getAction()) {
+                case ATTACK:
+                    if (sent) {
+                        if (player.getClientVersion().isOlderThanOrEquals(ClientVersion.V_1_8)) {
+                            if (flagAndAlert() && shouldModifyPackets()) {
+                                event.setCancelled(true);
+                                player.onPacketCancel();
+                            }
+                        } else {
+                            invalid++;
+                        }
+                    }
+                    break;
+                case INTERACT_AT:
+                case INTERACT:
+                    sent = true;
+            }
+        }
+
+        if (WrapperPlayClientPlayerFlying.isFlying(event.getPacketType()) && player.getClientVersion().isOlderThanOrEquals(ClientVersion.V_1_8) && !player.packetStateData.lastPacketWasTeleport) {
+            sent = false;
+        }
+    }
+
+    @Override
+    public void onPredictionComplete(PredictionComplete predictionComplete) {
+        // we don't need to check pre-1.9 players here (no tick skipping)
+        if (player.getClientVersion().isOlderThanOrEquals(ClientVersion.V_1_8)) return;
+
+        if (!player.skippedTickInActualMovement) {
+            for (; invalid >= 1; invalid--) {
+                flagAndAlert();
+            }
+        }
+
+        invalid = 0;
+        sent = false;
+    }
+}
