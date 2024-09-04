a89d83055e8205872f647fb1ccae7fdd2b3ad754
ManInMyVan <maninmyvan@gmail.com>
2024-09-04 15:40:34 -0600

    don't require implementing onBlockBreak

--- src/main/java/ac/grim/grimac/checks/type/BlockBreakCheck.java
+++ src/main/java/ac/grim/grimac/checks/type/BlockBreakCheck.java
@@ -3,5 +3,5 @@ package ac.grim.grimac.checks.type;
 import ac.grim.grimac.utils.anticheat.update.BlockBreak;
 
 public interface BlockBreakCheck extends PacketCheck {
-    void onBlockBreak(final BlockBreak blockBreak);
+    default void onBlockBreak(final BlockBreak blockBreak) {}
 }
