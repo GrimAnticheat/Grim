c1054142e08e6704ff2b25a542258cb567e8bb7e
Axionize <154778082+Axionize@users.noreply.github.com>
2024-10-03 00:01:05 -0400

    Banner Line Spacing/Styling
    Co-authored-by: ManInMyVan <113963219+ManInMyVan@users.noreply.github.com>

--- src/main/java/ac/grim/grimac/utils/collisions/CollisionData.java
+++ src/main/java/ac/grim/grimac/utils/collisions/CollisionData.java
@@ -263,8 +263,7 @@ public enum CollisionData {
 
     RAILS(NoCollisionBox.INSTANCE, BlockTags.RAILS.getStates().toArray(new StateType[0])),
 
-    BANNER(NoCollisionBox.INSTANCE,
-            BlockTags.BANNERS.getStates().toArray(new StateType[0])),
+    BANNER(NoCollisionBox.INSTANCE, BlockTags.BANNERS.getStates().toArray(new StateType[0])),
 
     SMALL_FLOWER(NoCollisionBox.INSTANCE,
             BlockTags.SMALL_FLOWERS.getStates().toArray(new StateType[0])),
