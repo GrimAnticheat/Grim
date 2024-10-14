b6df7a3a1addc26a25ca12ecc8ae7eadd0bef7c1
ManInMyVan <maninmyvan@gmail.com>
2024-10-14 14:55:13 -0600

    fix MultiBreak false

--- src/main/java/ac/grim/grimac/checks/impl/breaking/MultiBreak.java
+++ src/main/java/ac/grim/grimac/checks/impl/breaking/MultiBreak.java
@@ -61,7 +61,7 @@ public class MultiBreak extends Check implements BlockBreakCheck {
 
     @Override
     public void onPredictionComplete(PredictionComplete predictionComplete) {
-        if (player.getClientVersion().isNewerThan(ClientVersion.V_1_8) && !player.skippedTickInActualMovement) {
+        if (player.getClientVersion().isNewerThan(ClientVersion.V_1_8) && !player.skippedTickInActualMovement && predictionComplete.isChecked()) {
             for (String verbose : flags) {
                 flagAndAlert(verbose);
             }
