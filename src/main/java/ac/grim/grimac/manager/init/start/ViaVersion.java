09e6cec6efb919f6e33b2ee60c45e387b58d9f45
AoElite <20812471+AoElite@users.noreply.github.com>
2024-10-22 10:56:36 -0400

    fix older versions of via throwing an error due to PR

--- src/main/java/ac/grim/grimac/manager/init/start/ViaVersion.java
+++ src/main/java/ac/grim/grimac/manager/init/start/ViaVersion.java
@@ -8,12 +8,11 @@ import io.github.retrooper.packetevents.util.viaversion.ViaVersionUtil;
 public class ViaVersion implements Initable {
     @Override
     public void start() {
-        if (!ViaVersionUtil.isAvailable() || !Via.getConfig().fix1_21PlacementRotation()) {
-            return;
+        if (!ViaVersionUtil.isAvailable()) return;
+        if (Via.getConfig().getValues().containsKey("fix-1_21-placement-rotation") && Via.getConfig().fix1_21PlacementRotation()) {
+            LogUtil.warn("GrimAC has detected that you are using ViaVersion with the `fix-1_21-placement-rotation` option enabled.");
+            LogUtil.warn("This option is known to cause issues with GrimAC and may result in false positives and bypasses.");
+            LogUtil.warn("Please disable this option in your ViaVersion configuration to prevent these issues.");
         }
-
-        LogUtil.warn("GrimAC has detected that you are using ViaVersion with the `fix-1_21-placement-rotation` option enabled.");
-        LogUtil.warn("This option is known to cause issues with GrimAC and may result in false positives and bypasses.");
-        LogUtil.warn("Please disable this option in your ViaVersion configuration to prevent these issues.");
     }
 }
