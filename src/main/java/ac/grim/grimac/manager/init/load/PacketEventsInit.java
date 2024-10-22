4ffe6f4fc5a48bd747ce52cef5c447d799ea24ee
AoElite <20812471+AoElite@users.noreply.github.com>
2024-10-22 10:11:02 -0400

    packetevent optimizations

--- src/main/java/ac/grim/grimac/manager/init/load/PacketEventsInit.java
+++ src/main/java/ac/grim/grimac/manager/init/load/PacketEventsInit.java
@@ -4,7 +4,16 @@ import ac.grim.grimac.GrimAPI;
 import ac.grim.grimac.manager.init.Initable;
 import ac.grim.grimac.utils.anticheat.LogUtil;
 import com.github.retrooper.packetevents.PacketEvents;
+import com.github.retrooper.packetevents.protocol.chat.ChatTypes;
+import com.github.retrooper.packetevents.protocol.entity.data.EntityDataTypes;
+import com.github.retrooper.packetevents.protocol.entity.type.EntityTypes;
+import com.github.retrooper.packetevents.protocol.item.enchantment.type.EnchantmentTypes;
+import com.github.retrooper.packetevents.protocol.item.type.ItemTypes;
+import com.github.retrooper.packetevents.protocol.particle.type.ParticleTypes;
+import com.github.retrooper.packetevents.protocol.world.states.type.StateTypes;
 import io.github.retrooper.packetevents.factory.spigot.SpigotPacketEventsBuilder;
+
+import java.util.concurrent.Executors;
 
 public class PacketEventsInit implements Initable {
     @Override
@@ -16,7 +25,18 @@ public class PacketEventsInit implements Initable {
                 .fullStackTrace(true)
                 .kickOnPacketException(true)
                 .checkForUpdates(false)
+                .reEncodeByDefault(false)
                 .debug(false);
         PacketEvents.getAPI().load();
+        // This may seem useless, but it causes java to start loading stuff async before we need it
+        Executors.defaultThreadFactory().newThread(() -> {
+            StateTypes.AIR.getName();
+            ItemTypes.AIR.getName();
+            EntityTypes.PLAYER.getParent();
+            EntityDataTypes.BOOLEAN.getName();
+            ChatTypes.CHAT.getName();
+            EnchantmentTypes.ALL_DAMAGE_PROTECTION.getName();
+            ParticleTypes.DUST.getName();
+        }).start();
     }
 }
