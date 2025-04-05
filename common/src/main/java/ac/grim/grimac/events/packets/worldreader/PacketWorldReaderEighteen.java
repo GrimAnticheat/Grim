fc596e18c8da9ca905e67a6e80f56fa646c29998
Axionize <154778082+Axionize@users.noreply.github.com>
2025-04-05 15:39:04 -0400

    Fix 1.21.5 Support breaking with new Chunk format

--- common/src/main/java/ac/grim/grimac/events/packets/worldreader/PacketWorldReaderEighteen.java
+++ common/src/main/java/ac/grim/grimac/events/packets/worldreader/PacketWorldReaderEighteen.java
@@ -1,9 +1,11 @@
 package ac.grim.grimac.events.packets.worldreader;
 
 import ac.grim.grimac.player.GrimPlayer;
+import com.github.retrooper.packetevents.PacketEvents;
 import com.github.retrooper.packetevents.event.PacketSendEvent;
-import com.github.retrooper.packetevents.protocol.stream.NetStreamInput;
+import com.github.retrooper.packetevents.manager.server.ServerVersion;
 import com.github.retrooper.packetevents.protocol.world.chunk.BaseChunk;
+import com.github.retrooper.packetevents.protocol.world.chunk.HeightmapType;
 import com.github.retrooper.packetevents.protocol.world.chunk.impl.v_1_18.Chunk_v1_18;
 import com.github.retrooper.packetevents.protocol.world.chunk.reader.impl.ChunkReader_v1_18;
 import com.github.retrooper.packetevents.protocol.world.dimension.DimensionTypes;
