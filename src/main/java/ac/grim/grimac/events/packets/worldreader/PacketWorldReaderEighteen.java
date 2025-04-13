5b019f97980c548716e95ab7e8a88748975b7091
483378 <89095838+483378@users.noreply.github.com>
2025-04-13 22:33:41 +0020

    Fix PacketWorldReaderEighteen

--- src/main/java/ac/grim/grimac/events/packets/worldreader/PacketWorldReaderEighteen.java
+++ src/main/java/ac/grim/grimac/events/packets/worldreader/PacketWorldReaderEighteen.java
@@ -2,14 +2,11 @@ package ac.grim.grimac.events.packets.worldreader;
 
 import ac.grim.grimac.player.GrimPlayer;
 import com.github.retrooper.packetevents.event.PacketSendEvent;
-import com.github.retrooper.packetevents.protocol.stream.NetStreamInput;
 import com.github.retrooper.packetevents.protocol.world.chunk.BaseChunk;
 import com.github.retrooper.packetevents.protocol.world.chunk.impl.v_1_18.Chunk_v1_18;
 import com.github.retrooper.packetevents.protocol.world.chunk.reader.impl.ChunkReader_v1_18;
 import com.github.retrooper.packetevents.protocol.world.dimension.DimensionTypes;
 import com.github.retrooper.packetevents.wrapper.PacketWrapper;
-
-import java.io.ByteArrayInputStream;
 
 public class PacketWorldReaderEighteen extends BasePacketWorldReader {
 
@@ -24,10 +21,11 @@ public class PacketWorldReaderEighteen extends BasePacketWorldReader {
         // Skip past heightmaps
         wrapper.readNBT();
 
+        int dataLength = wrapper.readVarInt();
+
         BaseChunk[] chunks = new ChunkReader_v1_18().read(DimensionTypes.OVERWORLD,null,
                 null, true, false, false,
-                event.getUser().getTotalWorldHeight() >> 4, null,
-                new NetStreamInput(new ByteArrayInputStream(wrapper.readByteArray())));
+                event.getUser().getTotalWorldHeight() >> 4, dataLength, wrapper);
 
         // Remove biomes to save memory
         for (int i = 0; i < chunks.length; i++) {
