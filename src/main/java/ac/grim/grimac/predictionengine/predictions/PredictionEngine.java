95e97849fea17b35af3f99c328d38dc6ba610322
cyramek <xcyramek@gmail.com>
2025-04-16 15:54:56 +0200

    this is the right way

--- src/main/java/ac/grim/grimac/predictionengine/predictions/PredictionEngine.java
+++ src/main/java/ac/grim/grimac/predictionengine/predictions/PredictionEngine.java
@@ -434,7 +434,7 @@ public class PredictionEngine {
         }
 
         for (VectorData vector : velocities) {
-            if (player.getClientVersion().isNewerThanOrEquals(ClientVersion.V_1_21_5) && !player.inVehicle()) { // TODO: is this the right way??
+            if (player.getClientVersion().isNewerThanOrEquals(ClientVersion.V_1_21_5) && !player.inVehicle()) {
                 if (Collisions.getHorizontalDistanceSqr(vector.vector) < 9.0E-6) {
                     vector.vector.setX(0D);
                     vector.vector.setZ(0D);
