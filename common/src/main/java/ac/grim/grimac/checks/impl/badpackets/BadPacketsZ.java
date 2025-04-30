65f76216139ad55a221d4bea880eae72030ab269
Rejomy <a@a.a>
2025-04-30 20:36:41 +0500

    Fix false positives

--- common/src/main/java/ac/grim/grimac/checks/impl/badpackets/BadPacketsZ.java
+++ common/src/main/java/ac/grim/grimac/checks/impl/badpackets/BadPacketsZ.java
@@ -21,12 +21,16 @@ public class BadPacketsZ extends Check implements PacketCheck {
         if (event.getPacketType() == PacketType.Play.Client.CLICK_WINDOW) {
             WrapperPlayClientClickWindow wrapper = new WrapperPlayClientClickWindow(event);
             int slotId = wrapper.getSlot();
-            boolean clickOutsideInventory = slotId == -999;
+            int inventorySize = player.getInventory().menu.getSlots().size();
+            // -999 called when player click outside inventory, but if he does it with block, the slot id should be -1.
+            boolean clickOutsideInventory = slotId == -999 || slotId == -1;
             boolean impossibleNegativeSlotId = !clickOutsideInventory && slotId < 0;
-            boolean higherThanMaxSlotSizeSlotId = slotId >= player.getInventory().getPacketSendingInventorySize();
+            // Furnace can produce 0 inventory slot size.
+            boolean validInventorySize = inventorySize > 0;
+            boolean higherThanMaxSlotSizeSlotId = slotId >= inventorySize;
 
-            if (impossibleNegativeSlotId || higherThanMaxSlotSizeSlotId) {
-                if (flagAndAlert("slotId=" + slotId) && shouldModifyPackets())
+            if (impossibleNegativeSlotId || validInventorySize && higherThanMaxSlotSizeSlotId) {
+                if (flagAndAlert("slotId=" + slotId + " size=" + inventorySize) && shouldModifyPackets())
                     // Cancel packet with invalid slot id.
                     event.setCancelled(true);
             }
