9705bf763f2f71ccba759573f5d135e15723c7c5
BenchmarkMode <217130189+BenchmarkMode@users.noreply.github.com>
2025-07-15 23:23:12 +0100

    add hasSentViaProxyPacket field

--- common/src/main/java/ac/grim/grimac/player/GrimPlayer.java
+++ common/src/main/java/ac/grim/grimac/player/GrimPlayer.java
@@ -274,6 +274,7 @@ public class GrimPlayer implements GrimUser {
     public final LongSet visitedBlocks = new LongOpenHashSet();
     private @Nullable UserConnection viaUserConnection;
     public boolean wasLastPredictionCompleteChecked;
+    public boolean hasSentViaProxyPacket;
     public boolean isJumping = false, lastJumping = false;
 
     public GrimPlayer(@NonNull User user) {
