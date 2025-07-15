4a74da810973a0c07f6ec24b78e1f15d27c0d588
BenchmarkMode <217130189+BenchmarkMode@users.noreply.github.com>
2025-07-15 03:45:00 +0100

    register PacketPluginMessage

--- common/src/main/java/ac/grim/grimac/manager/init/start/PacketManager.java
+++ common/src/main/java/ac/grim/grimac/manager/init/start/PacketManager.java
@@ -1,20 +1,6 @@
 package ac.grim.grimac.manager.init.start;
 
-import ac.grim.grimac.events.packets.CheckManagerListener;
-import ac.grim.grimac.events.packets.PacketBlockAction;
-import ac.grim.grimac.events.packets.PacketEntityAction;
-import ac.grim.grimac.events.packets.PacketHidePlayerInfo;
-import ac.grim.grimac.events.packets.PacketPingListener;
-import ac.grim.grimac.events.packets.PacketPlayerAttack;
-import ac.grim.grimac.events.packets.PacketPlayerCooldown;
-import ac.grim.grimac.events.packets.PacketPlayerDigging;
-import ac.grim.grimac.events.packets.PacketPlayerJoinQuit;
-import ac.grim.grimac.events.packets.PacketPlayerRespawn;
-import ac.grim.grimac.events.packets.PacketPlayerSteer;
-import ac.grim.grimac.events.packets.PacketSelfMetadataListener;
-import ac.grim.grimac.events.packets.PacketServerTags;
-import ac.grim.grimac.events.packets.PacketServerTeleport;
-import ac.grim.grimac.events.packets.ProxyAlertMessenger;
+import ac.grim.grimac.events.packets.*;
 import ac.grim.grimac.events.packets.worldreader.BasePacketWorldReader;
 import ac.grim.grimac.events.packets.worldreader.PacketWorldReaderEight;
 import ac.grim.grimac.events.packets.worldreader.PacketWorldReaderEighteen;
@@ -37,6 +23,7 @@ public class PacketManager implements StartableInitable {
         PacketEvents.getAPI().getEventManager().registerListener(new PacketSelfMetadataListener());
         PacketEvents.getAPI().getEventManager().registerListener(new PacketServerTeleport());
         PacketEvents.getAPI().getEventManager().registerListener(new PacketPlayerCooldown());
+        PacketEvents.getAPI().getEventManager().registerListener(new PacketPluginMessage());
         PacketEvents.getAPI().getEventManager().registerListener(new PacketPlayerRespawn());
         PacketEvents.getAPI().getEventManager().registerListener(new CheckManagerListener());
         PacketEvents.getAPI().getEventManager().registerListener(new PacketPlayerSteer());
