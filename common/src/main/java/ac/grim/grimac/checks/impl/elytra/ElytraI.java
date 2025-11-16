8ac3ab142db1c45857a59a4e7cb811e3648295d9
DieInCalamity <infinityblockssupp0rt@gmail.com>
2025-11-16 13:14:27 +0700

    Mark elytra checks stable

--- common/src/main/java/ac/grim/grimac/checks/impl/elytra/ElytraI.java
+++ common/src/main/java/ac/grim/grimac/checks/impl/elytra/ElytraI.java
@@ -10,7 +10,7 @@ import com.github.retrooper.packetevents.protocol.packettype.PacketType;
 import com.github.retrooper.packetevents.protocol.player.ClientVersion;
 import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientEntityAction;
 
-@CheckData(name = "ElytraI", description = "Started gliding in water", experimental = true)
+@CheckData(name = "ElytraI", description = "Started gliding in water")
 public class ElytraI extends Check implements PostPredictionCheck {
     private boolean setback;
 
