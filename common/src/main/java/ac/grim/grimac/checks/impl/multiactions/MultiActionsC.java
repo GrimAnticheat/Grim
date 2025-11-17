ac868f2429850574e2bf1bd506e9f21d373834e2
DieInCalamity <160578963+DieInCalamity@users.noreply.github.com>
2025-11-18 03:05:39 +0700

    Remove wrong import

--- common/src/main/java/ac/grim/grimac/checks/impl/multiactions/MultiActionsC.java
+++ common/src/main/java/ac/grim/grimac/checks/impl/multiactions/MultiActionsC.java
@@ -4,7 +4,6 @@ import ac.grim.grimac.checks.Check;
 import ac.grim.grimac.checks.CheckData;
 import ac.grim.grimac.checks.type.PacketCheck;
 import ac.grim.grimac.player.GrimPlayer;
-import ac.grim.grimac.utils.data.types.Position;
 import com.github.retrooper.packetevents.event.PacketReceiveEvent;
 import com.github.retrooper.packetevents.protocol.packettype.PacketType;
 import com.github.retrooper.packetevents.protocol.player.ClientVersion;
