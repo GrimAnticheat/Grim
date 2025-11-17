345841b23e9f414a57b1be0720251fd32cfabe36
DieInCalamity <160578963+DieInCalamity@users.noreply.github.com>
2025-11-18 03:22:49 +0700

    Mark MultiActionsD as experimental

--- common/src/main/java/ac/grim/grimac/checks/impl/multiactions/MultiActionsD.java
+++ common/src/main/java/ac/grim/grimac/checks/impl/multiactions/MultiActionsD.java
@@ -7,7 +7,7 @@ import ac.grim.grimac.player.GrimPlayer;
 import com.github.retrooper.packetevents.event.PacketReceiveEvent;
 import com.github.retrooper.packetevents.protocol.packettype.PacketType;
 
-@CheckData(name = "MultiActionsD", description = "Closed inventory while moving")
+@CheckData(name = "MultiActionsD", description = "Closed inventory while moving", experimental = true)
 public class MultiActionsD extends Check implements PacketCheck {
     public MultiActionsD(GrimPlayer player) {
         super(player);
