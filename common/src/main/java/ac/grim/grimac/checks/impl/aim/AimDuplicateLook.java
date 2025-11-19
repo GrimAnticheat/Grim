9d4849de2735e4a556f430968e984cc92e59727c
DieInCalamity <160578963+DieInCalamity@users.noreply.github.com>
2025-11-19 22:09:06 +0700

    build

--- common/src/main/java/ac/grim/grimac/checks/impl/aim/AimDuplicateLook.java
+++ common/src/main/java/ac/grim/grimac/checks/impl/aim/AimDuplicateLook.java
@@ -6,7 +6,7 @@ import ac.grim.grimac.checks.type.RotationCheck;
 import ac.grim.grimac.player.GrimPlayer;
 import ac.grim.grimac.utils.anticheat.update.RotationUpdate;
 
-@CheckData(name = "AimDuplicateLook")
+@CheckData(name = "AimDuplicateLook") // build
 public class AimDuplicateLook extends Check implements RotationCheck {
     private boolean exempt;
 
