41d3b3dabb4745834f7375fd76515fe2a568fc7e
DieInCalamity <160578963+DieInCalamity@users.noreply.github.com>
2025-12-01 03:17:59 +0700

    BlockHitData and ReachUtilsPrimitives

---
+++ common/src/main/java/ac/grim/grimac/utils/data/BlockHitData.java
@@ -0,0 +1,30 @@
+package ac.grim.grimac.utils.data;
+
+import com.github.retrooper.packetevents.protocol.world.BlockFace;
+import com.github.retrooper.packetevents.protocol.world.states.WrappedBlockState;
+import com.github.retrooper.packetevents.util.Vector3d;
+import com.github.retrooper.packetevents.util.Vector3i;
+import lombok.Getter;
+import lombok.ToString;
+import org.bukkit.util.Vector;
+
+@Getter
+@ToString
+public class BlockHitData extends HitData {
+    Vector3i position;
+    WrappedBlockState state;
+    BlockFace closestDirection;
+    public Boolean success;
+
+    public BlockHitData(Vector3i position, Vector blockHitLocation, BlockFace closestDirection, WrappedBlockState state, Boolean success) {
+        super(blockHitLocation);
+        this.position = position;
+        this.closestDirection = closestDirection;
+        this.state = state;
+        this.success = success;
+    }
+
+    public Vector3d getRelativeBlockHitLocation() {
+        return new Vector3d(blockHitLocation.getX() - position.getX(), blockHitLocation.getY() - position.getY(), blockHitLocation.getZ() - position.getZ());
+    }
+}
