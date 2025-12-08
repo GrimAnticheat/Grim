62cc39d4a0ae851c259d0719efbb3ad60d5bdf29
Rothes <449181985@qq.com>
2025-12-08 17:25:10 +0800

    Use ConcurrentLinkedQueue for concurrent

--- common/src/main/java/ac/grim/grimac/utils/latency/LatencyUtils.java
+++ common/src/main/java/ac/grim/grimac/utils/latency/LatencyUtils.java
@@ -7,17 +7,12 @@ import ac.grim.grimac.utils.anticheat.MessageUtil;
 import ac.grim.grimac.utils.common.arguments.CommonGrimArguments;
 import ac.grim.grimac.utils.data.Pair;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.ListIterator;
+import java.util.Iterator;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class LatencyUtils {
-    private final LinkedList<Pair<Integer, Runnable>> transactionMap = new LinkedList<>();
+    private final ConcurrentLinkedQueue<Pair<Integer, Runnable>> transactionMap = new ConcurrentLinkedQueue<>();
     private final GrimPlayer player;
-
-    // Built from transactionMap and cleared at start of every handleNettySyncTransaction() call
-    // The actual usage scope of this variable's use is limited to within the synchronized block of handleNettySyncTransaction
-    private final ArrayList<Runnable> tasksToRun = new ArrayList<>();
 
     public LatencyUtils(GrimPlayer player) {
         this.player = player;
@@ -40,62 +35,33 @@ public class LatencyUtils {
             }
             return;
         }
-        synchronized (this) {
-            transactionMap.add(new Pair<>(transaction, runnable));
-        }
+        transactionMap.add(new Pair<>(transaction, runnable));
     }
 
     public void handleNettySyncTransaction(int transaction) {
-        /*
-         * This code uses a two-pass approach within the synchronized block to prevent CMEs.
-         * First we collect and remove tasks using the iterator, then execute all collected tasks.
-         *
-         * The issue:
-         *     We cannot execute tasks during iteration because if a runnable modifies transactionMap
-         *     or calls addRealTimeTask, it will cause a ConcurrentModificationException.
-         *     While only seen on Folia servers, this is theoretically possible everywhere.
-         *
-         * Why this solution:
-         *     Rather than documenting "don't modify transactionMap in runnables" and risking subtle
-         *     bugs from future contributions or Check API usage, we prevent the issue entirely
-         *     at a small performance cost.
-         *
-         * Future considerations:
-         *     If this becomes a performance bottleneck, we may revisit using a single-pass approach
-         *     on non-Folia servers. We could also explore concurrent data structures or parallel
-         *     execution, but this would lose the guarantee that transactions are processed in order.
-         */
-        synchronized (this) {
-            tasksToRun.clear();
+        // First pass: collect tasks and mark them for removal
+        Iterator<Pair<Integer, Runnable>> iterator = transactionMap.iterator();
+        while (iterator.hasNext()) {
+            Pair<Integer, Runnable> pair = iterator.next();
 
-            // First pass: collect tasks and mark them for removal
-            ListIterator<Pair<Integer, Runnable>> iterator = transactionMap.listIterator();
-            while (iterator.hasNext()) {
-                Pair<Integer, Runnable> pair = iterator.next();
+            // We are at most a tick ahead when running tasks based on transactions, meaning this is too far
+            if (transaction + 1 < pair.first())
+                break;
 
-                // We are at most a tick ahead when running tasks based on transactions, meaning this is too far
-                if (transaction + 1 < pair.first())
-                    break;
+            // This is at most tick ahead of what we want
+            if (transaction == pair.first() - 1)
+                continue;
 
-                // This is at most tick ahead of what we want
-                if (transaction == pair.first() - 1)
-                    continue;
-
-                tasksToRun.add(pair.second());
-                iterator.remove();
-            }
-
-            for (Runnable runnable : tasksToRun) {
-                try {
-                    runnable.run();
-                } catch (Exception e) {
-                    LogUtil.error("An error has occurred when running transactions for player: " + player.user.getName(), e);
-                    // Kick the player SO PEOPLE ACTUALLY REPORT PROBLEMS AND KNOW WHEN THEY HAPPEN
-                    if (CommonGrimArguments.KICK_ON_TRANSACTION_ERRORS.value()) {
-                        player.disconnect(MessageUtil.miniMessage(MessageUtil.replacePlaceholders(player, GrimAPI.INSTANCE.getConfigManager().getDisconnectPacketError())));
-                    }
+            try {
+                pair.second().run();
+            } catch (Exception e) {
+                LogUtil.error("An error has occurred when running transactions for player: " + player.user.getName(), e);
+                // Kick the player SO PEOPLE ACTUALLY REPORT PROBLEMS AND KNOW WHEN THEY HAPPEN
+                if (CommonGrimArguments.KICK_ON_TRANSACTION_ERRORS.value()) {
+                    player.disconnect(MessageUtil.miniMessage(MessageUtil.replacePlaceholders(player, GrimAPI.INSTANCE.getConfigManager().getDisconnectPacketError())));
                 }
             }
+            iterator.remove();
         }
     }
 }
