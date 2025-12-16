350c773f0a5293440999c60ecdf6b2a137260e49
DieInCalamity <160578963+DieInCalamity@users.noreply.github.com>
2025-12-16 22:25:57 +0700

    Patch a noslow

--- common/src/main/java/ac/grim/grimac/checks/impl/badpackets/BadPacketsH.java
+++ common/src/main/java/ac/grim/grimac/checks/impl/badpackets/BadPacketsH.java
@@ -1,5 +1,6 @@
 package ac.grim.grimac.checks.impl.badpackets;
 
+import ac.grim.grimac.GrimAPI;
 import ac.grim.grimac.checks.CheckData;
 import ac.grim.grimac.checks.type.BlockPlaceCheck;
 import ac.grim.grimac.player.GrimPlayer;
