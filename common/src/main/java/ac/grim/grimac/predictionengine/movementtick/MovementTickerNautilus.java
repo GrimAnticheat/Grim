49a16664aa63f65875919a541691a839e2790706
cyramek <xcyramek@gmail.com>
2025-12-29 19:23:42 +0100

    refactor nautilus prediction engines

--- common/src/main/java/ac/grim/grimac/predictionengine/movementtick/MovementTickerNautilus.java
+++ common/src/main/java/ac/grim/grimac/predictionengine/movementtick/MovementTickerNautilus.java
@@ -1,12 +1,9 @@
 package ac.grim.grimac.predictionengine.movementtick;
 
 import ac.grim.grimac.player.GrimPlayer;
-import ac.grim.grimac.predictionengine.predictions.rideable.PredictionEngineNautilusLava;
-import ac.grim.grimac.predictionengine.predictions.rideable.PredictionEngineNautilusNormal;
 import ac.grim.grimac.predictionengine.predictions.rideable.PredictionEngineNautilusWater;
 import ac.grim.grimac.utils.data.packetentity.PacketEntityNautilus;
 import ac.grim.grimac.utils.math.Vector3dm;
-import ac.grim.grimac.utils.nmsutil.BlockProperties;
 import com.github.retrooper.packetevents.protocol.attribute.Attributes;
 
 public class MovementTickerNautilus extends MovementTickerLivingVehicle {
@@ -41,18 +38,8 @@ public class MovementTickerNautilus extends MovementTickerLivingVehicle {
     }
 
     @Override
-    public void doNormalMove(float blockFriction) {
-        new PredictionEngineNautilusNormal(this.movementInput).guessBestMovement(BlockProperties.getFrictionInfluencedSpeed(blockFriction, player), player);
-    }
-
-    @Override
     public void doWaterMove(float swimSpeed, boolean isFalling, float swimFriction) {
         new PredictionEngineNautilusWater(this.movementInput, 0.9).guessBestMovement(getRiddenSpeed(player), player);
-    }
-
-    @Override
-    public void doLavaMove() {
-        new PredictionEngineNautilusLava(movementInput).guessBestMovement(0.02F, player);
     }
 
     public float getRiddenSpeed(GrimPlayer player) {
--- common/src/main/java/ac/grim/grimac/predictionengine/predictions/rideable/PredictionEngineRideableNormal.java
+++ common/src/main/java/ac/grim/grimac/predictionengine/predictions/rideable/PredictionEngineRideableNormal.java
@@ -3,6 +3,7 @@ package ac.grim.grimac.predictionengine.predictions.rideable;
 import ac.grim.grimac.player.GrimPlayer;
 import ac.grim.grimac.predictionengine.predictions.PredictionEngineNormal;
 import ac.grim.grimac.utils.data.VectorData;
+import ac.grim.grimac.utils.math.GrimMath;
 import ac.grim.grimac.utils.math.Vector3dm;
 import lombok.RequiredArgsConstructor;
 
@@ -20,6 +21,18 @@ public class PredictionEngineRideableNormal extends PredictionEngineNormal {
 
     @Override
     public List<VectorData> applyInputsToVelocityPossibilities(GrimPlayer player, Set<VectorData> possibleVectors, float speed) {
-        return PredictionEngineRideableUtils.applyInputsToVelocityPossibilities(movementVector, player, possibleVectors, speed);
+        return PredictionEngineRideableUtils.applyInputsToVelocityPossibilities(this, movementVector, player, possibleVectors, speed);
+    }
+
+    @Override
+    public Vector3dm getMovementResultFromInput(GrimPlayer player, Vector3dm inputVector, float flyingSpeed, float yRot) {
+        float yRotRadians = GrimMath.radians(yRot);
+        float sin = player.trigHandler.sin(yRotRadians);
+        float cos = player.trigHandler.cos(yRotRadians);
+
+        double xResult = inputVector.getX() * cos - inputVector.getZ() * sin;
+        double zResult = inputVector.getZ() * cos + inputVector.getX() * sin;
+
+        return new Vector3dm(xResult * flyingSpeed, inputVector.getY() * flyingSpeed, zResult * flyingSpeed);
     }
 }
