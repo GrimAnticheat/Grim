6f4715dbbf31fc642dbae7c87d3f00f04300c377
cyramek <xcyramek@gmail.com>
2026-01-07 04:27:53 +0100

    remove outdated comment regarding nautilus jumping logic

--- common/src/main/java/ac/grim/grimac/predictionengine/predictions/rideable/PredictionEngineRideableUtils.java
+++ common/src/main/java/ac/grim/grimac/predictionengine/predictions/rideable/PredictionEngineRideableUtils.java
@@ -24,7 +24,7 @@ public final class PredictionEngineRideableUtils {
         // TODO: onGround can desync if it's first riding tick
         jumpable.executeJump(player, possibleVectors);
 
-        // More jumping stuff // TODO the code below probably does not apply to nautilus
+        // More jumping stuff
         boolean legacyJumpingMechanics = player.getClientVersion().isOlderThanOrEquals(ClientVersion.V_1_19_3);
         boolean onGround = legacyJumpingMechanics
                 ? player.clientControlledVerticalCollision
