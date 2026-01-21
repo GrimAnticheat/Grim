2d2d4389226446ec85403ba09e443d38e0a8e500
LaserSlime <76707643+LaserSlime@users.noreply.github.com>
2026-01-21 17:03:17 +0100

    Fix respawns

--- common/src/main/java/ac/grim/grimac/events/packets/PacketSelfMetadataListener.java
+++ common/src/main/java/ac/grim/grimac/events/packets/PacketSelfMetadataListener.java
@@ -90,8 +90,6 @@ public class PacketSelfMetadataListener extends PacketListenerAbstract {
                             player.lastSprinting = isSprinting;
                             if(!isSprinting) {
                                 player.camelSprintingState = SprintingState.STOPPING;
-                            } else {
-                                player.camelSprintingState = SprintingState.STARTED; // Not sure if this is needed but just in case...
                             }
                             // Protect this due to players being able to get the server to spam this packet a lot
                             if (player.isGliding != isGliding) {
