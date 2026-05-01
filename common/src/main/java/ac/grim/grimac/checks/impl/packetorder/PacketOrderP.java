af88e6681d907fbdf99c8a4cb9ba89ccea9b0187
ManInMyVan <113963219+ManInMyVan@users.noreply.github.com>
2026-04-30 19:04:03 -0600

    stablekey

--- common/src/main/java/ac/grim/grimac/checks/impl/packetorder/PacketOrderP.java
+++ common/src/main/java/ac/grim/grimac/checks/impl/packetorder/PacketOrderP.java
@@ -14,7 +14,7 @@ import org.jetbrains.annotations.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
-@CheckData(name = "PacketOrderP", description = "Sent keepalive and transaction packets out of order", experimental = true)
+@CheckData(name = "PacketOrderP", stableKey = "grim.packetorder.transaction_keepalive_order", description = "Sent keepalive and transaction packets out of order", experimental = true)
 public class PacketOrderP extends Check implements PacketCheck {
 
     private final List<KeepAliveData> pendingKeepAlives = new ArrayList<>();
