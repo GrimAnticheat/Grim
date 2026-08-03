16ea437234ebe8762124e9f18562bb7c6ba16f63
ManInMyVan <113963219+ManInMyVan@users.noreply.github.com>
2026-08-03 14:40:50 -0600

    optimize BadPacketsJ

--- common/src/main/java/ac/grim/grimac/checks/impl/badpackets/BadPacketsJ.java
+++ common/src/main/java/ac/grim/grimac/checks/impl/badpackets/BadPacketsJ.java
@@ -4,7 +4,6 @@ import ac.grim.grimac.checks.Check;
 import ac.grim.grimac.checks.CheckData;
 import ac.grim.grimac.checks.type.PacketCheck;
 import ac.grim.grimac.player.GrimPlayer;
-import ac.grim.grimac.utils.data.HeadRotation;
 import com.github.retrooper.packetevents.PacketEvents;
 import com.github.retrooper.packetevents.event.PacketReceiveEvent;
 import com.github.retrooper.packetevents.manager.server.ServerVersion;
@@ -12,12 +11,12 @@ import com.github.retrooper.packetevents.protocol.packettype.PacketType;
 import com.github.retrooper.packetevents.protocol.player.ClientVersion;
 import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientUseItem;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @CheckData(name = "BadPacketsJ", stableKey = "grim.badpackets.use_item_rotation_mismatch", description = "Rotation in use item packet did not match tick rotation")
 public class BadPacketsJ extends Check implements PacketCheck {
-    private final List<HeadRotation> rotations = new ArrayList<>();
+
+    private float yaw;
+    private float pitch;
+    private int rotations;
 
     public BadPacketsJ(GrimPlayer player) {
         super(player);
@@ -32,32 +31,40 @@ public class BadPacketsJ extends Check implements PacketCheck {
     @Override
     public void onPacketReceive(PacketReceiveEvent event) {
         if (!player.cameraEntity.isSelf()) {
-            rotations.clear();
+            this.rotations = 0;
             return;
         }
 
         if (event.getPacketType() == PacketType.Play.Client.USE_ITEM) {
             WrapperPlayClientUseItem packet = new WrapperPlayClientUseItem(event);
-            rotations.add(new HeadRotation(packet.getYaw(), packet.getPitch()));
-        }
 
-        if (isTickPacket(event.getPacketType())) {
-            // due to tick skipping, the rotations sent could be last tick's
-            boolean allowLast = player.canSkipTicks() && (event.getPacketType() == PacketType.Play.Client.PLAYER_POSITION_AND_ROTATION || event.getPacketType() == PacketType.Play.Client.PLAYER_ROTATION);
-            for (HeadRotation rotation : rotations) {
-                if (rotation.yaw() == player.yaw && rotation.pitch() == player.pitch) {
-                    allowLast = false;
-                    continue;
+            float yaw = packet.getYaw();
+            float pitch = packet.getPitch();
+
+            if (this.rotations > 0 && (this.yaw != yaw || this.pitch != pitch)) {
+                if (!player.canSkipTicks() || this.yaw != player.yaw || this.pitch != player.pitch) {
+                    while (this.rotations-- > 0) flag();
                 }
-
-                if (rotation.yaw() == player.lastYaw && rotation.pitch() == player.lastPitch && allowLast) {
-                    continue;
-                }
-
-                flag();
+                this.rotations = 0;
             }
 
-            rotations.clear();
+            if (this.rotations != Integer.MAX_VALUE)
+                this.rotations++;
+            this.yaw = yaw;
+            this.pitch = pitch;
+        }
+
+        if (this.rotations > 0 && isTickPacket(event.getPacketType())) {
+            if (this.yaw != player.yaw || this.pitch != player.pitch) {
+                // due to tick skipping, the rotations sent could be last tick's
+                boolean allowLast = player.canSkipTicks()
+                        && (event.getPacketType() == PacketType.Play.Client.PLAYER_POSITION_AND_ROTATION
+                        || event.getPacketType() == PacketType.Play.Client.PLAYER_ROTATION);
+                if (!allowLast || this.yaw != player.lastYaw || this.pitch != player.lastPitch) {
+                    while (this.rotations-- > 0) flag();
+                }
+            }
+            this.rotations = 0;
         }
     }
 }
