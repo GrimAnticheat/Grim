9662e876f5e1b547ff17ccb12278c25876214728
PalorderSoftWorksOfficial <palordersoftworks@gmail.com>
2026-08-29 09:17:23 +0200

    fix(fabric): align mc261 Adventure dependencies with 5.x

--- fabric/official/mc261/build.gradle.kts
+++ fabric/official/mc261/build.gradle.kts
@@ -3,4 +3,6 @@ val minecraft_version: String by project
 
 dependencies {
     minecraft("com.mojang:minecraft:$minecraft_version")
+    implementation("net.kyori:adventure-api:5.2.0")
+    implementation("net.kyori:adventure-text-serializer-gson:5.2.0")
 }
